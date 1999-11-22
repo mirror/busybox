0f4c0848c7057dd696d82f9a0ca5cc8749a4131a
Eric Andersen <andersen@codepoet.org>
1999-11-22 05:57:09 +0000

    Fix bugs

--- install.sh
+++ install.sh
@@ -6,18 +6,15 @@ if [ "$1" == "" ]; then
     echo "No installation directory, aborting."
     exit 1;
 fi
-rm -rf $1
 
 h=`sort busybox.links | uniq`
 
 for i in $h ; do
-	echo "working on $i now"
+	echo "Symlinking $i to /bin/busybox"
 	mypath=`echo $i | sed -e 's/\(^.*\/\)\(.*\)/\1/g' `;
 	myapp=`echo $i | sed -e 's/\(^.*\/\)\(.*\)/\2/g' `;
-	echo "mkdir -p $1$mypath"
-	echo "(cd $1$mypath ; ln -s /bin/busybox $1$mypath$myapp )"
 	mkdir -p $1$mypath
-	(cd $1$mypath ; ln -s /bin/busybox $1$mypath$myapp )
+	(cd $1$mypath ; rm -f $1$mypath$myapp ; ln -s /bin/busybox $1$mypath$myapp )
 done
 rm -f $1/bin/busybox
 install -m 755 busybox $1/bin/busybox
