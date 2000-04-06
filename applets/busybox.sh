e9fee653dcceacb65b965f77dc20437cd862d976
Erik Andersen <andersen@codepoet.org>
2000-04-06 00:10:39 +0000

    Use gcc -E instead of cpp to enhance portability for brain damaged platforms (i.e. OpenLinux) that do not have cpp in the default path but instead hide it under /usr/lib/...  -Erik

--- applets/busybox.sh
+++ applets/busybox.sh
@@ -3,7 +3,7 @@
 # I added in the extra "ls" so only source files that
 # actually exist will show up in the compile list.
 ls -1 ` \
-    cpp busybox.def.h -dM | \
+    gcc -E -dM busybox.def.h | \
     sed -n -e '/^.*BB_FEATURE.*$/d;s/^#define.*\<BB_\(.*\)\>/\1.c/gp;' \
     | tr [:upper:] [:lower:] | sort
 ` 2>/dev/null | sed -e 's/\.c$/\.o/g'
