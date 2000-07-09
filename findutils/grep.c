e9b527ae48cb9609ea0668974de32da523ebd231
Eric Andersen <andersen@codepoet.org>
2000-07-09 05:56:14 +0000

    include getopt.h  -Erik

--- findutils/grep.c
+++ findutils/grep.c
@@ -22,7 +22,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> /* for getopt() */
+#include <getopt.h>
 #include <regex.h>
 #include <string.h> /* for strerror() */
 #include <errno.h>
