5d513f72568afe4552cfc12880b7744c88bfdd4b
Pavel Roskin <proski@gnu.org>
2000-09-13 14:03:48 +0000

    List of signals updated from util-linux-2.10o Architecture names are no longer used - should now compile on SPARC

--- procps/kill.c
+++ procps/kill.c
@@ -39,62 +39,83 @@ struct signal_name {
 };
 
 const struct signal_name signames[] = {
-	/* Everything, order not important */
-	{"HUP", SIGHUP},
-	{"INT", SIGINT},
-	{"QUIT", SIGQUIT},
-	{"ILL", SIGILL},
-	{"TRAP", SIGTRAP},
-	{"ABRT", SIGABRT},
-	{"FPE", SIGFPE},
-	{"KILL", SIGKILL},
-	{"SEGV", SIGSEGV},
-	{"PIPE", SIGPIPE},
-	{"ALRM", SIGALRM},
-	{"TERM", SIGTERM},
-	{"BUS", SIGBUS},
-	{"USR1", SIGUSR1},
-	{"USR2", SIGUSR2},
-	{"STOP", SIGSTOP},
-	{"CONT", SIGCONT},
-	{"TTIN", SIGTTIN},
-	{"TTOU", SIGTTOU},
-	{"IO", SIGIO},
-	{"TSTP", SIGTSTP},
-	{"CHLD", SIGCHLD},
-	{"XCPU", SIGXCPU},
-	{"XFSZ", SIGXFSZ},
-	{"PROF", SIGPROF},
-	{"WINCH", SIGWINCH},
-	{"URG", SIGURG},
-	{"VTALRM", SIGVTALRM},
-#ifndef __alpha__
-	/* everything except alpha */
-	{"IOT", SIGIOT},
-	{"POLL", SIGPOLL},
+	/* POSIX signals */
+	{ "HUP",	SIGHUP },	/* 1 */
+	{ "INT",	SIGINT }, 	/* 2 */
+	{ "QUIT",	SIGQUIT }, 	/* 3 */
+	{ "ILL",	SIGILL }, 	/* 4 */
+	{ "ABRT",	SIGABRT }, 	/* 6 */
+	{ "FPE",	SIGFPE }, 	/* 8 */
+	{ "KILL",	SIGKILL }, 	/* 9 */
+	{ "SEGV",	SIGSEGV }, 	/* 11 */
+	{ "PIPE",	SIGPIPE }, 	/* 13 */
+	{ "ALRM",	SIGALRM }, 	/* 14 */
+	{ "TERM",	SIGTERM }, 	/* 15 */
+	{ "USR1",	SIGUSR1 }, 	/* 10 (arm,i386,m68k,ppc), 30 (alpha,sparc*), 16 (mips) */
+	{ "USR2",	SIGUSR2 }, 	/* 12 (arm,i386,m68k,ppc), 31 (alpha,sparc*), 17 (mips) */
+	{ "CHLD",	SIGCHLD }, 	/* 17 (arm,i386,m68k,ppc), 20 (alpha,sparc*), 18 (mips) */
+	{ "CONT",	SIGCONT }, 	/* 18 (arm,i386,m68k,ppc), 19 (alpha,sparc*), 25 (mips) */
+	{ "STOP",	SIGSTOP },	/* 19 (arm,i386,m68k,ppc), 17 (alpha,sparc*), 23 (mips) */
+	{ "TSTP",	SIGTSTP },	/* 20 (arm,i386,m68k,ppc), 18 (alpha,sparc*), 24 (mips) */
+	{ "TTIN",	SIGTTIN },	/* 21 (arm,i386,m68k,ppc,alpha,sparc*), 26 (mips) */
+	{ "TTOU",	SIGTTOU },	/* 22 (arm,i386,m68k,ppc,alpha,sparc*), 27 (mips) */
+	/* Miscellaneous other signals */
+#ifdef SIGTRAP
+	{ "TRAP",	SIGTRAP },	/* 5 */
 #endif
-#if defined(__sparc__) || defined(__alpha__) || defined(__mips__)
-	/* everthing except intel */
-	{"EMT", SIGEMT},
-	{"SYS", SIGSYS},
-# ifdef __alpha__
-		/* alpha only */
-		{"LOST", SIGLOST},
+#ifdef SIGIOT
+	{ "IOT",	SIGIOT }, 	/* 6, same as SIGABRT */
 #endif
-#ifdef __sparc__
-		/* space only */
-		{"INFO", SIGINFO},
+#ifdef SIGEMT
+	{ "EMT",	SIGEMT }, 	/* 7 (mips,alpha,sparc*) */
 #endif
-#ifdef __mips__
-		/* mips only */
-		{"CLD", SIGCLD},
-		{"PWR", SIGPWR},
+#ifdef SIGBUS
+	{ "BUS",	SIGBUS },	/* 7 (arm,i386,m68k,ppc), 10 (mips,alpha,sparc*) */
 #endif
-#else
-	/* intel only */
-	{"STKFLT", SIGSTKFLT},
-	{"PWR", SIGPWR},
-	{"UNUSED", SIGUNUSED},
+#ifdef SIGSYS
+	{ "SYS",	SIGSYS }, 	/* 12 (mips,alpha,sparc*) */
+#endif
+#ifdef SIGSTKFLT
+	{ "STKFLT",	SIGSTKFLT },	/* 16 (arm,i386,m68k,ppc) */
+#endif
+#ifdef SIGURG
+	{ "URG",	SIGURG },	/* 23 (arm,i386,m68k,ppc), 16 (alpha,sparc*), 21 (mips) */
+#endif
+#ifdef SIGIO
+	{ "IO",		SIGIO },	/* 29 (arm,i386,m68k,ppc), 23 (alpha,sparc*), 22 (mips) */
+#endif
+#ifdef SIGPOLL
+	{ "POLL",	SIGPOLL },	/* same as SIGIO */
+#endif
+#ifdef SIGCLD
+	{ "CLD",	SIGCLD },	/* same as SIGCHLD (mips) */
+#endif
+#ifdef SIGXCPU
+	{ "XCPU",	SIGXCPU },	/* 24 (arm,i386,m68k,ppc,alpha,sparc*), 30 (mips) */
+#endif
+#ifdef SIGXFSZ
+	{ "XFSZ",	SIGXFSZ },	/* 25 (arm,i386,m68k,ppc,alpha,sparc*), 31 (mips) */
+#endif
+#ifdef SIGVTALRM
+	{ "VTALRM",	SIGVTALRM },	/* 26 (arm,i386,m68k,ppc,alpha,sparc*), 28 (mips) */
+#endif
+#ifdef SIGPROF
+	{ "PROF",	SIGPROF },	/* 27 (arm,i386,m68k,ppc,alpha,sparc*), 29 (mips) */
+#endif
+#ifdef SIGPWR
+	{ "PWR",	SIGPWR },	/* 30 (arm,i386,m68k,ppc), 29 (alpha,sparc*), 19 (mips) */
+#endif
+#ifdef SIGINFO
+	{ "INFO",	SIGINFO },	/* 29 (alpha) */
+#endif
+#ifdef SIGLOST
+	{ "LOST",	SIGLOST }, 	/* 29 (arm,i386,m68k,ppc,sparc*) */
+#endif
+#ifdef SIGWINCH
+	{ "WINCH",	SIGWINCH },	/* 28 (arm,i386,m68k,ppc,alpha,sparc*), 20 (mips) */
+#endif
+#ifdef SIGUNUSED
+	{ "UNUSED",	SIGUNUSED },	/* 31 (arm,i386,m68k,ppc) */
 #endif
 	{0, 0}
 };
