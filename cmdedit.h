849083c8862b1d7d1246e50b38f453edcec3a58c
Eric Andersen <andersen@codepoet.org>
2001-01-04 11:09:00 +0000

    Update interface per what now exists...

--- cmdedit.h
+++ cmdedit.h
@@ -2,24 +2,11 @@
 #define CMDEDIT_H
 
 #ifdef BB_FEATURE_SH_COMMAND_EDITING
-/* unix systems can #define POSIX to use termios, otherwise 
- * the bsd or sysv interface will be used 
- */
-
 #include <stddef.h>
-
-typedef size_t (*cmdedit_strwidth_proc)(char *);
 
 void cmdedit_init(void);
 void cmdedit_terminate(void);
 void cmdedit_read_input(char* promptStr, char* command);		/* read a line of input */
-void cmdedit_setwidth(int);		/* specify width of screen */
-void cmdedit_histadd(char *);		/* adds entries to hist */
-void cmdedit_strwidth(cmdedit_strwidth_proc);	/* to bind cmdedit_strlen */
-
-extern int 	(*cmdedit_in_hook)(char *);
-extern int 	(*cmdedit_out_hook)(char *);
-extern int	(*cmdedit_tab_hook)(char *, int, int *);
 
 #endif /* BB_FEATURE_SH_COMMAND_EDITING */
 
