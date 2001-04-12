a6ce670a87ee77ccb9337ed6d87442134e1a48ed
Glenn L McGrath <bug1@ihug.co.nz>
2001-04-12 02:26:04 +0000

    use tmpfile() and revert my previous changes... convert() belongs here

--- coreutils/dos2unix.c
+++ coreutils/dos2unix.c
@@ -27,8 +27,92 @@
  * See the COPYING file for license information.
  */
 
+#include <string.h>
 #include <getopt.h>
 #include "busybox.h"
+
+// if fn is NULL then input is stdin and output is stdout
+extern int convert(char *fn, int ConvType) {
+	char c;
+	char *tempFn = NULL;
+	FILE *in = stdin, *out = stdout;
+
+	if (fn != NULL) {
+		if ((in = wfopen(fn, "r")) == NULL) {
+			return -1;
+		}
+		if ((out = tmpfile()) == NULL) {
+			perror_msg(NULL);
+			return -2;
+		}
+	}
+
+	while ((c = fgetc(in)) != EOF) {
+		if (c == '\r') {
+			if ((ConvType == CT_UNIX2DOS) && (fn != NULL)) {
+				// file is alredy in DOS format so it is not necessery to touch it
+				if (fclose(in) < 0 || fclose(out) < 0) {
+					perror_msg(NULL);
+					return -2;
+				}
+				return 0;
+			}
+			if (!ConvType)
+				ConvType = CT_DOS2UNIX;
+			break;
+		}
+		if (c == '\n') {
+			if ((ConvType == CT_DOS2UNIX) && (fn != NULL)) {
+				// file is alredy in UNIX format so it is not necessery to touch it
+				if ((fclose(in) < 0) || (fclose(out) < 0)) {
+					perror_msg(NULL);
+					return -2;
+				}
+				return 0;
+			}
+			if (!ConvType) {
+				ConvType = CT_UNIX2DOS;
+			}
+			if (ConvType == CT_UNIX2DOS) {
+				fputc('\r', out);
+			}
+			fputc('\n', out);
+			break;
+		}
+		fputc(c, out);
+	}
+	if (c != EOF)
+		while ((c = fgetc(in)) != EOF) {
+			if (c == '\r')
+				continue;
+			if (c == '\n') {
+				if (ConvType == CT_UNIX2DOS)
+					fputc('\r', out);
+				fputc('\n', out);
+				continue;
+			}
+		fputc(c, out);
+	}
+
+	if (fn != NULL) {
+	    if (fclose(in) < 0 || fclose(out) < 0 || 
+		    (in = fopen(tempFn, "r")) == NULL || (out = fopen(fn, "w")) == NULL) {
+			perror_msg(NULL);
+			return -2;
+	    }
+
+	    while ((c = fgetc(in)) != EOF) {
+			fputc(c, out);
+		}
+
+	    if ((fclose(in) < 0) || (fclose(out) < 0)) {
+			perror_msg(NULL);
+			return -2;
+	    }
+	}
+
+	return 0;
+}
 
 int dos2unix_main(int argc, char *argv[]) {
 	int ConvType = CT_AUTO;
