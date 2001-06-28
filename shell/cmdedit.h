184d73636d2bbf334b39087a10618e5339b6bd18
Eric Andersen <andersen@codepoet.org>
2001-06-28 03:22:19 +0000

    Remove unneeded stuff.

--- shell/cmdedit.h
+++ shell/cmdedit.h
@@ -1,14 +1,8 @@
 #ifndef CMDEDIT_H
 #define CMDEDIT_H
 
-#ifdef BB_FEATURE_COMMAND_EDITING
-#include <stddef.h>
-#include "busybox.h"
-
 void cmdedit_init(void);
 void cmdedit_terminate(void);
 void cmdedit_read_input(char* promptStr, char* command);		/* read a line of input */
-
-#endif /* BB_FEATURE_COMMAND_EDITING */
 
 #endif /* CMDEDIT_H */
