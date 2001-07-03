623cfa99e5b247c0a08b83acfb46a2399a5be957
Matt Kraai <kraai@debian.org>
2001-07-03 20:55:30 +0000

    Remove a redundant call to mkdir.

--- archival/tar.c
+++ archival/tar.c
@@ -399,16 +399,8 @@ tarExtractDirectory(TarInfo *header, int extractFlag, int tostdoutFlag)
 	if (extractFlag==FALSE || tostdoutFlag==TRUE)
 		return( TRUE);
 
-	if (make_directory(header->name, header->mode, FILEUTILS_RECUR) < 0) {
+	if (make_directory(header->name, header->mode, FILEUTILS_RECUR) < 0)
 		return( FALSE);
-	}
-	/* make the final component, just in case it was
-	 * omitted by make_directory() (which will skip the
-	 * directory if it doesn't have a terminating '/') */
-	if (mkdir(header->name, header->mode) < 0 && errno != EEXIST) {
-		perror_msg("%s", header->name);
-		return FALSE;
-	}
 
 	fixUpPermissions(header);
 	return( TRUE);
