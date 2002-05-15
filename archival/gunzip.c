081df62b921cf17f1d4c4e0214abfa273c92cfde
Robert Griebl <griebl@gmx.de>
2002-05-15 21:45:52 +0000

    Don't delete source file when decompressing to stdout This is the normal GNU gunzip behaviour

--- archival/gunzip.c
+++ archival/gunzip.c
@@ -132,7 +132,8 @@ static int gunzip_file (const char *path, int flags)
     /* do the decompression, and cleanup */
     if (unzip(in_file, out_file) == 0) {
 	/* Success, remove .gz file */
-	delete_path = path;
+	if ( !(flags & gunzip_to_stdout ))
+	    delete_path = path;
 	if (flags & gunzip_verbose) {
 	    fprintf(stderr, "OK\n");
 	}
