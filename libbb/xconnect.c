cafc10323099befd1e45bb1d212a511dcbb92b11
Eric Andersen <andersen@codepoet.org>
2002-07-11 10:40:43 +0000

    Brad Campbell <brad@seme.com.au> notes that xconnect.c needs #include <netinet/in.h> to compile if CONFIG_FEATURE_IPV6 is not defined

--- libbb/xconnect.c
+++ libbb/xconnect.c
@@ -8,10 +8,11 @@
 
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-#include <stdlib.h>
+#include <netinet/in.h>
 #include "libbb.h"
 
 int xconnect(const char *host, const char *port)
