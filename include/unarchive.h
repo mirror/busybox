4176972db5a65af78d13fed97ab84bf4fceaee2c
Glenn L McGrath <bug1@ihug.co.nz>
2002-08-22 13:25:12 +0000

    Run through indent, use /* */ style comments

--- include/unarchive.h
+++ include/unarchive.h
@@ -1,5 +1,5 @@
-#include <stdio.h>	// for FILE
-#include <unistd.h>	// for off_t
+#include <stdio.h>		/* for FILE */
+#include <unistd.h>		/* for off_t */
 
 enum extract_functions_e {
 	extract_verbose_list = 1,
@@ -26,20 +26,23 @@ typedef struct file_headers_s {
 	mode_t mode;
 	time_t mtime;
 	dev_t device;
-	int (*extract_func)(FILE *, FILE *);
+	int (*extract_func) (FILE *, FILE *);
 } file_header_t;
 
-file_header_t *get_header_ar(FILE *in_file);
-file_header_t *get_header_cpio(FILE *src_stream);
-file_header_t *get_header_tar(FILE *tar_stream);
-file_header_t *get_header_zip(FILE *zip_stream);
+file_header_t *get_header_ar(FILE * in_file);
+file_header_t *get_header_cpio(FILE * src_stream);
+file_header_t *get_header_tar(FILE * tar_stream);
+file_header_t *get_header_zip(FILE * zip_stream);
 
-void seek_sub_file(FILE *src_stream, const int count);
+void seek_sub_file(FILE * src_stream, const int count);
 
 extern off_t archive_offset;
 
-char *unarchive(FILE *src_stream, FILE *out_stream, file_header_t *(*get_headers)(FILE *),
-	const int extract_function, const char *prefix, char **include_name, char **exclude_name);
+char *unarchive(FILE * src_stream, FILE * out_stream,
+				file_header_t * (*get_headers) (FILE *),
+				const int extract_function, const char *prefix,
+				char **include_name, char **exclude_name);
 
-char *deb_extract(const char *package_filename, FILE *out_stream, const int extract_function,
-	const char *prefix, const char *filename);
+char *deb_extract(const char *package_filename, FILE * out_stream,
+				  const int extract_function, const char *prefix,
+				  const char *filename);
