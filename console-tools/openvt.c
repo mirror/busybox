ebdc8b48a563615f6e0b08bdf2c91241fed669e0
Glenn L McGrath <bug1@ihug.co.nz>
2002-09-16 03:47:48 +0000

    Put in GPL v2 or later copyright notice

--- console-tools/openvt.c
+++ console-tools/openvt.c
@@ -1,9 +1,22 @@
 /* vi: set sw=4 ts=4: */
-
 /*
- * openvt.c - open a vt to run a command.
+ *  openvt.c - open a vt to run a command.
  *
- * busyboxed by Quy Tonthat <quy@signal3.com>
+ *  busyboxed by Quy Tonthat <quy@signal3.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /* getopt not needed */
