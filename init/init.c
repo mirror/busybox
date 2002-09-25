a920871c3ae0f83ccf9be4c314448cece719b3df
Eric Andersen <andersen@codepoet.org>
2002-09-25 15:08:40 +0000

    Ignoring SIGCHLD causes a race leading to the occasional hang of init when init will wait() on itself in waitfor() when the child exits before init is scheduled to run.  Letting init hang is very seriously bad.  -Erik

--- init/init.c
+++ init/init.c
@@ -1063,7 +1063,6 @@ extern int init_main(int argc, char **argv)
 	signal(SIGCONT, cont_handler);
 	signal(SIGSTOP, stop_handler);
 	signal(SIGTSTP, stop_handler);
-	signal(SIGCHLD, SIG_IGN);
 
 	/* Turn off rebooting via CTL-ALT-DEL -- we get a 
 	 * SIGINT on CAD so we can shut things down gracefully... */
