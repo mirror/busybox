b8e556e5f54f2848bf1ad246189cb2bf44b643bd
Glenn L McGrath <bug1@ihug.co.nz>
2002-11-03 11:46:49 +0000

    Add copyright notice and license, use strcat+strcpy instead of sprintf

--- archival/libunarchive/data_extract_all_prefix.c
+++ archival/libunarchive/data_extract_all_prefix.c
@@ -1,11 +1,23 @@
-#include <sys/types.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <stdlib.h>
+/*
+ *  Copyright (C) by Glenn McGrath <bug1@optushome.com.au>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 #include <string.h>
-#include <utime.h>
-#include <unistd.h>
-#include <stdlib.h>
+
 #include "libbb.h"
 #include "unarchive.h"
 
@@ -16,7 +28,8 @@ extern void data_extract_all_prefix(archive_handle_t *archive_handle)
 	name_ptr += strspn(name_ptr, "./");
 	if (name_ptr[0] != '\0') {
 		archive_handle->file_header->name = xmalloc(strlen(archive_handle->buffer) + 2 + strlen(name_ptr));
-		sprintf(archive_handle->file_header->name, "%s%s", archive_handle->buffer, name_ptr);
+		strcpy(archive_handle->file_header->name, archive_handle->buffer);
+		strcat(archive_handle->file_header->name, name_ptr);
 		data_extract_all(archive_handle);
 	}
 	return;
