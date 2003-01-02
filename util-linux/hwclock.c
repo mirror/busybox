7ce75f44ae8cfb419adca01593eb5b055ccc4b16
Robert Griebl <griebl@gmx.de>
2003-01-02 07:16:53 +0000

    > Eliminate dependancy on kernel header files.  Naughty naughty. > Using linux kernel headers is a Bad Thing(tm) and should be > punished. >  -Erik
    Doing this wrong should be punished even harder :)
     - Robert

--- util-linux/hwclock.c
+++ util-linux/hwclock.c
@@ -35,8 +35,21 @@
 
 
 /* Copied from linux/rtc.h to eliminate the kernel dependancy */
-#define RTC_SET_TIME	_IOW('p', 0x0a, struct tm) /* Set RTC time    */
-#define RTC_RD_TIME	_IOR('p', 0x09, struct tm) /* Read RTC time   */
+struct linux_rtc_time {
+	int tm_sec;
+	int tm_min;
+	int tm_hour;
+	int tm_mday;
+	int tm_mon;
+	int tm_year;
+	int tm_wday;
+	int tm_yday;
+	int tm_isdst;
+};
+                                    
+
+#define RTC_SET_TIME   _IOW('p', 0x0a, struct linux_rtc_time) /* Set RTC time    */
+#define RTC_RD_TIME    _IOR('p', 0x09, struct linux_rtc_time) /* Read RTC time   */
 
 
 #ifdef CONFIG_FEATURE_HWCLOCK_LONGOPTIONS
