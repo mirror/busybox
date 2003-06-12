fa7532b91a6aa649532678eb3b30909a9691cafe
Eric Andersen <andersen@codepoet.org>
2003-06-12 22:09:08 +0000

    Fix the location for the default config so 'make defconfig' actually works

--- scripts/config/confdata.c
+++ scripts/config/confdata.c
@@ -17,7 +17,7 @@
 
 const char conf_def_filename[] = ".config";
 
-const char conf_defname[] = "extra/Configs/Config.$TARGET_ARCH.default";
+const char conf_defname[] = "sysdeps/linux/defconfig";
 
 const char *conf_confnames[] = {
 	".config",
