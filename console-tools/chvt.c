28672dd3b9ff9d27c771f011089e38a2928aded7
Eric Andersen <andersen@codepoet.org>
2003-10-22 10:30:53 +0000

    Patch from Arthur Othieno for docs/style-guide.txt conformance.

--- console-tools/chvt.c
+++ console-tools/chvt.c
@@ -1,11 +1,25 @@
 /* vi: set sw=4 ts=4: */
 /*
- * chvt.c - aeb - 940227 - Change virtual terminal
+ * Mini chvt implementation for busybox
  *
- * busyboxed by Erik Andersen
+ * Copyright (C) 1999-2003 by Erik Andersen <andersen@codepoet.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-/* getopt not needed */
+/* no options, no getopt */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,14 +36,18 @@ int chvt_main(int argc, char **argv)
 {
 	int fd, num;
 
-	if ((argc != 2) || (**(argv + 1) == '-'))
+	if ((argc != 2) || (**(argv + 1) == '-')) {
 		bb_show_usage();
+	}
+
 	fd = get_console_fd();
 	num = atoi(argv[1]);
-	if (ioctl(fd, VT_ACTIVATE, num))
+	if (ioctl(fd, VT_ACTIVATE, num)) {
 		bb_perror_msg_and_die("VT_ACTIVATE");
-	if (ioctl(fd, VT_WAITACTIVE, num))
+	}
+	if (ioctl(fd, VT_WAITACTIVE, num)) {
 		bb_perror_msg_and_die("VT_WAITACTIVE");
+	}
 	return EXIT_SUCCESS;
 }
 
