40c9489d336ad42451b8eb124f82ff14958d3945
Glenn L McGrath <bug1@ihug.co.nz>
2003-10-30 22:51:33 +0000

    Add a comment with link to the SUSv3 standard for xargs

--- findutils/xargs.c
+++ findutils/xargs.c
@@ -23,6 +23,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
+ * xargs is described in the Single Unix Specification v3 at
+ * http://www.opengroup.org/onlinepubs/007904975/utilities/xargs.html
+ *
  */
 
 #include <stdio.h>
