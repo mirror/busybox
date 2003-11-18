20872be9a4e670666b307f94b8fca6bf807c9b7b
Glenn L McGrath <bug1@ihug.co.nz>
2003-11-18 21:31:19 +0000

    Dont close original file handle, we may need it later.

--- archival/libunarchive/open_transformer.c
+++ archival/libunarchive/open_transformer.c
@@ -36,15 +36,16 @@ extern int open_transformer(int src_fd, int (*transformer)(int src_fd, int dst_f
 
 	if (pid == 0) {
 		/* child process */
-	    close(fd_pipe[0]); /* We don't wan't to read from the pipe */
+	    close(fd_pipe[0]); /* We don't wan't to read from the parent */
 	    transformer(src_fd, fd_pipe[1]);
 	    close(fd_pipe[1]); /* Send EOF */
+		close(src_fd);
 	    exit(0);
 	    /* notreached */
 	}
+
 	/* parent process */
-	close(fd_pipe[1]); /* Don't want to write down the pipe */
-	close(src_fd);
+	close(fd_pipe[1]); /* Don't want to write to the child */
 
 	return(fd_pipe[0]);
 }
