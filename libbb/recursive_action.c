66a56aa028b430a3d20ac47c20dccf2aad3b21a0
Eric Andersen <andersen@codepoet.org>
2004-04-07 17:59:04 +0000

    Vladimir N. Oleynik writes:
    Hi.
    
    Last changes (rev 1.12) to recursive_actions() by Christian Grigis
    have problem.
    Test for demonstrate:
    
    $ mkdir aaa bbb ccc
    $ su
    # chown root bbb
    # chmod 700 bbb
    # exit
    $ busybox chmod 777 -R .
    ./bbb: Permision denied
    
    But "./ccc" mode not changed. Previous variant works fine,
    errors skiped and continued recursion.
    
    
    --w
    vodz

--- libbb/recursive_action.c
+++ libbb/recursive_action.c
@@ -100,7 +100,7 @@ int recursive_action(const char *fileName,
 			return FALSE;
 		}
 		status = TRUE;
-		while (status && (next = readdir(dir)) != NULL) {
+		while ((next = readdir(dir)) != NULL) {
 			char *nextFile;
 
 			nextFile = concat_subpath_file(fileName, next->d_name);
