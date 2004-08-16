cf6ef0571122d99f45adb782f1181c7abca61ce3
Eric Andersen <andersen@codepoet.org>
2004-08-16 08:29:44 +0000

    Aurelien Jacobs writes:
    with a quick conversion you will see that 132608 == 0x20600
    so noticed that the elif will never be matched !
    Apparently there was already a try to modify this in CVS which
    was reverted (it was plain wrong).
    
    I don't know when __kernel_old_dev_t is needed, but with a 2.6.7
    or a 2.6.8 this is __kernel_dev_t wich is needed.
    
    I corrected this with the following patch but maybe older 2.6
    still need __kernel_old_dev_t ?
    
    I think this should be corrected before 1.0.
    
    Thanks
    Aurel

--- libbb/loop.c
+++ libbb/loop.c
@@ -32,9 +32,7 @@
 #include <linux/version.h>
 #include <asm/posix_types.h>
 
-#if LINUX_VERSION_CODE >= 132608
-#define __bb_kernel_dev_t   __kernel_old_dev_t
-#elif LINUX_VERSION_CODE >= 0x20600
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,0)
 #define __bb_kernel_dev_t   __kernel_dev_t
 #else
 #define __bb_kernel_dev_t   unsigned short
