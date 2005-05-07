cce1ae2ce541563eb70dcb181b5031fe1ad98e4a
Rob Landley <rob@landley.net>
2005-05-07 07:53:16 +0000

    Shaun Jackman pointed out that dprintf(STDOUT_FILENO,...) is just a printf.

--- coreutils/tail.c
+++ coreutils/tail.c
@@ -62,7 +62,8 @@ static int status
 static void tail_xprint_header(const char *fmt, const char *filename)
 {
 	/* If we get an output error, there is really no sense in continuing. */
-	if (dprintf(STDOUT_FILENO, fmt, filename) < 0) {
+	if (printf(fmt, filename) < 0 ||
+			fflush(stdout) < 0) {
 		bb_perror_nomsg_and_die();
 	}
 }
