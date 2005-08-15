c8e2de6e1b183adbb6bb944e7eb8bd7012db3ba1
Mike Frysinger <vapier@gentoo.org>
2005-08-15 22:13:21 +0000

    Bernhard Fischer writes: fix devfsd usage

--- include/usage.h
+++ include/usage.h
@@ -395,10 +395,10 @@
 #endif
 
 #define devfsd_trivial_usage \
-	"mntpnt [-v]" \
+	"devfsd [-v]" \
 	USAGE_DEVFSD_FG_NP("[-fg][-np]" )
 #define devfsd_full_usage \
-	"Optional daemon for managing devfs permissions and old device name symlinks.\n" \
+	"Obsolete daemon for managing devfs permissions and old device name symlinks.\n" \
 	"\nOptions:\n" \
 	"\tmntpnt\tThe mount point where devfs is mounted.\n\n" \
 	"\t-v\tPrint the protocol version numbers for devfsd\n" \
