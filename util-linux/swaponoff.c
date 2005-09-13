6943a9489ebdf93460e3e0500938af02f08530bf
Mike Frysinger <vapier@gentoo.org>
2005-09-13 02:29:39 +0000

    whitespace/syntax cleanups, dont mind me :p

--- util-linux/swaponoff.c
+++ util-linux/swaponoff.c
@@ -4,20 +4,7 @@
  *
  * Copyright (C) 1999-2004 by Erik Andersen <andersen@codepoet.org>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
+ * Licensed under the GPL v2, see the file LICENSE in this tarball.
  */
 
 #include <stdio.h>
@@ -36,16 +23,13 @@ static int swap_enable_disable(const char *device)
 	int status;
 	struct stat st;
 
-	if (stat(device, &st) < 0) {
+	if (stat(device, &st) < 0)
 		bb_perror_msg_and_die("cannot stat %s", device);
-	}
 
 	/* test for holes */
-	if (S_ISREG(st.st_mode)) {
-		if (st.st_blocks * 512 < st.st_size) {
+	if (S_ISREG(st.st_mode))
+		if (st.st_blocks * 512 < st.st_size)
 			bb_error_msg_and_die("swap file has holes");
-		}
-	}
 
 	if (bb_applet_name[5] == 'n')
 		status = swapon(device, 0);
@@ -56,40 +40,42 @@ static int swap_enable_disable(const char *device)
 		bb_perror_msg("%s", device);
 		return EXIT_FAILURE;
 	}
-	/*printf("%s: %s\n", bb_applet_name, device);*/
+
 	return EXIT_SUCCESS;
 }
 
 static int do_em_all(void)
 {
 	struct mntent *m;
-	FILE *f = setmntent("/etc/fstab", "r");
-	int err = 0;
+	FILE *f;
+	int err;
 
+	f = setmntent("/etc/fstab", "r");
 	if (f == NULL)
 		bb_perror_msg_and_die("/etc/fstab");
-	while ((m = getmntent(f)) != NULL) {
-		if (strcmp(m->mnt_type, MNTTYPE_SWAP)==0) {
-			if(swap_enable_disable(m->mnt_fsname) == EXIT_FAILURE)
-				err++;
-		}
-	}
+
+	err = 0;
+	while ((m = getmntent(f)) != NULL)
+		if (strcmp(m->mnt_type, MNTTYPE_SWAP) == 0)
+			if (swap_enable_disable(m->mnt_fsname) == EXIT_FAILURE)
+				++err;
+
 	endmntent(f);
+
 	return err;
 }
 
-#define DO_ALL      1
+#define DO_ALL    0x01
 
 extern int swap_on_off_main(int argc, char **argv)
 {
-	unsigned long opt = bb_getopt_ulflags (argc, argv, "a");
-	
-	if (argc != 2) {
+	unsigned long opt = bb_getopt_ulflags(argc, argv, "a");
+
+	if (argc != 2)
 		bb_show_usage();
-	}
-	
+
 	if (opt & DO_ALL)
 		return do_em_all();
-	
+
 	return swap_enable_disable(argv[1]);
 }
