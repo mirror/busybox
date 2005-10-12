ffe17b51b4305519d39e6772ce0bc7c564efede7
"Vladimir N. Oleynik" <dzo@simtreas.ru>
2005-10-12 08:38:28 +0000

    correct strange *3 isatty(0) code

--- console-tools/reset.c
+++ console-tools/reset.c
@@ -30,12 +30,12 @@
 
 extern int reset_main(int argc, char **argv)
 {
-	if (isatty(0) || isatty(0) || isatty(0)) {
+	if (isatty(1)) {
 		/* See 'man 4 console_codes' for details:
 		 * "ESC c"			-- Reset
 		 * "ESC ( K"		-- Select user mapping
 		 * "ESC [ J"		-- Erase display
-		 * "ESC [ 0 m"		-- Reset all Graphics Rendition display attributes
+		 * "ESC [ 0 m"		-- Reset all display attributes
 		 * "ESC [ ? 25 h"	-- Make cursor visible.
 		 */
 		printf("\033c\033(K\033[J\033[0m\033[?25h");
