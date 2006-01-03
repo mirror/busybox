705fad296447295ef4e71e301c5b7cdbf186d6a1
Mike Frysinger <vapier@gentoo.org>
2006-01-03 23:59:17 +0000

    just use bb_xfopen()

--- modutils/lsmod.c
+++ modutils/lsmod.c
@@ -169,10 +169,7 @@ extern int lsmod_main(int argc, char **argv)
 	  FILE *file;
 	  char line[4096];
 
-	  file = fopen("/proc/modules", "r");
-
-	  if (!file) 
-	    bb_error_msg_and_die("Opening /proc/modules");
+	  file = bb_xfopen("/proc/modules", "r");
 
 	  while (fgets(line, sizeof(line), file)) {
 	    char *tok;
