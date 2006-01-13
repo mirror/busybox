4a1865ca5eaf6bebc76ef8066c19e95df0edc7c3
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2006-01-13 18:11:59 +0000

    - revert last change for now..

--- util-linux/losetup.c
+++ util-linux/losetup.c
@@ -3,20 +3,7 @@
  *
  * Copyright (C) 2002  Matt Kraai.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
+ * Licensed under GPLv2 or later, see file LICENSE in this tarball for details.
  */
 
 #include <getopt.h>
@@ -39,7 +26,7 @@ die_failed:
       bb_perror_msg_and_die("%s",argv[optind]);
 
     case 'o':
-      offset = atol(optarg);
+      offset = bb_xparse_number (optarg, NULL);
       /* Fall through to do the losetup */
     case -1:
       /* losetup takes two argument:, loop_device and file */
