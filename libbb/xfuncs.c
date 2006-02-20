4e9deec8bf465552ff621a80421721b702051747
Rob Landley <rob@landley.net>
2006-02-20 02:44:30 +0000

    svn 14077 was completely broken and apparently never tested.

--- libbb/xfuncs.c
+++ libbb/xfuncs.c
@@ -4,19 +4,7 @@
  *
  * Copyright (C) 1999-2004 by Erik Andersen <andersen@codepoet.org>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * Licensed under GPLv2 or later, see file LICENSE in this tarball for details.
  */
 
 #include <sys/types.h>
@@ -179,7 +167,9 @@ extern void bb_xfflush_stdout(void)
 #endif
 
 #ifdef L_strlen
-/* Stupid gcc always includes its own builtin strlen()... */
+/* Since gcc always inlines strlen(), this saves a byte or two, but we need
+ * the #undef here to avoid endless loop from #define strlen bb_strlen */
+#undef strlen
 size_t bb_strlen(const char *string)
 {
 	    return(strlen(string));
