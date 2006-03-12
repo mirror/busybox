e01d746fe962e1c3de57f8b909abc9801ecceff1
Rob Landley <rob@landley.net>
2006-03-12 19:26:01 +0000

    Cleanup patch from tito.

--- coreutils/who.c
+++ coreutils/who.c
@@ -21,48 +21,45 @@
 #include <time.h>
 #include "busybox.h"
 
+static const char * idle_string (time_t t)
+{
+	static char str[6];
+	
+	time_t s = time(NULL) - t;
+
+	if (s < 60)
+		return ".";
+	if (s < (24 * 60 * 60)) {
+		sprintf (str, "%02d:%02d",
+				(int) (s / (60 * 60)),
+				(int) ((s % (60 * 60)) / 60));
+		return (const char *) str;
+	}
+	return "old";
+}
+
 int who_main(int argc, char **argv)
 {
-    struct utmp *ut;
-    struct stat st;
-    time_t     idle;
-    char *name;
-
-    if (argc > 1)
-	bb_show_usage();
-
-    setutent();
-    printf("USER       TTY      IDLE      FROM           HOST\n");
-
-    while ((ut = getutent()) != NULL) {
-
-	if (ut->ut_user[0] && ut->ut_type == USER_PROCESS) {
-		/* ut->ut_line is device name of tty - "/dev/" */
-	    printf("%-10s %-8s ", ut->ut_user, ut->ut_line);
-
-		name = concat_path_file("/dev/", ut->ut_line);
-	    if (stat(name, &st) == 0) {
-		idle = time(NULL) -  st.st_atime;
-
-		if (idle < 60)
-		    printf("00:00m    ");
-		else if (idle < (60 * 60))
-		    printf("00:%02dm    ", (int)(idle / 60));
-		else if (idle < (24 * 60 * 60))
-		    printf("%02d:%02dm    ", (int)(idle / (60 * 60)),
-			   (int)(idle % (60 * 60)) / 60);
-		else if (idle < (24 * 60 * 60 * 365))
-		    printf("%03ddays   ", (int)(idle / (24 * 60 * 60)));
-		else
-		    printf("%02dyears   ", (int) (idle / (24 * 60 * 60 * 365)));
-	    } else
-		printf("%-8s  ", "?");
-
-	    printf("%-12.12s   %s\n", ctime((time_t*)&(ut->ut_tv.tv_sec)) + 4, ut->ut_host);
-		free(name);
+	struct utmp *ut;
+	struct stat st;
+	char *name;
+	
+	if (argc > 1) {
+		bb_show_usage();
 	}
-    }
-    endutent();
-
-    return 0;
+	
+	setutent();
+	printf("USER       TTY      IDLE      TIME           HOST\n");
+	while ((ut = getutent()) != NULL) {
+		if (ut->ut_user[0] && ut->ut_type == USER_PROCESS) {
+			/* ut->ut_line is device name of tty - "/dev/" */
+			name = concat_path_file("/dev", ut->ut_line);
+			printf("%-10s %-8s %-8s  %-12.12s   %s\n", ut->ut_user, ut->ut_line,
+									(stat(name, &st)) ?  "?" : idle_string(st.st_atime),
+									ctime((time_t*)&(ut->ut_tv.tv_sec)) + 4, ut->ut_host);
+			free(name);
+		}
+	}
+	endutent();
+	return 0;
 }
