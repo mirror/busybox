787140df3992ccc3ebdc09f6d2dcb584f580f49f
Mike Frysinger <vapier@gentoo.org>
2006-03-23 23:44:29 +0000

    remove in place of external link

--- networking/udhcp/clientsocket.h
+++
@@ -1,7 +0,0 @@
-/* clientsocket.h */
-#ifndef _CLIENTSOCKET_H
-#define _CLIENTSOCKET_H
-
-int raw_socket(int ifindex);
-
-#endif
--- networking/udhcp/script.h
+++
@@ -1,6 +0,0 @@
-#ifndef _SCRIPT_H
-#define _SCRIPT_H
-
-extern void run_script(struct dhcpMessage *packet, const char *name);
-
-#endif
