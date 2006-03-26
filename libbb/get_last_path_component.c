3570915769efcc520f0f14b094c1d4c4e137fa6c
Rob Landley <rob@landley.net>
2006-03-26 21:49:42 +0000

    More dead code removal.

--- libbb/get_last_path_component.c
+++ libbb/get_last_path_component.c
@@ -4,39 +4,13 @@
  *
  * Copyright (C) 2001  Manuel Novoa III  <mjn3@codepoet.org>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
+ * Licensed under GPLv2 or later, see file LICENSE in this tarball for details.
  */
-
-/* Set to 1 if you want basename() behavior for NULL or "". */
-/* WARNING!!! Doing so will break basename applet at least! */
-#define EMULATE_BASENAME	0
 
 char *bb_get_last_path_component(char *path)
 {
-#if EMULATE_BASENAME
-	static const char null_or_empty[] = ".";
-#endif
 	char *first = path;
 	char *last;
-
-#if EMULATE_BASENAME
-	if (!path || !*path) {
-		return (char *) null_or_empty;
-	}
-#endif
 
 	last = path - 1;
 
