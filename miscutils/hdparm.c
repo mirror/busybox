061fd0a353ef05de4a96e23c18e25316babee523
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2006-04-05 16:37:25 +0000

    - make sure that the label expected_hwif_error is visible even when   CONFIG_FEATURE_HDPARM_HDIO_SCAN_HWIF isn't set.

--- miscutils/hdparm.c
+++ miscutils/hdparm.c
@@ -7,21 +7,7 @@
  *
  * Hacked by Tito <farmatito@tiscali.it> for size optimization.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
- * 02111-1307 USA
- *
+ * Licensed under GPLv2 or later, see file LICENSE in this tarball for details.
  *
  * This program is based on the source code of hdparm: see below...
  * hdparm.c - Command line interface to get/set hard disk parameters
@@ -2785,8 +2771,10 @@ missing_arg_error:
 						if (argc && isdigit(**argv))
 							p = *argv++, --argc;
 						else
+#endif /* CONFIG_FEATURE_HDPARM_HDIO_SCAN_HWIF */
 expected_hwif_error:
 							bb_error_msg_and_die("expected hwif value"); /* "expected hwif_irq" */
+#ifdef CONFIG_FEATURE_HDPARM_HDIO_SCAN_HWIF
 
 						sscanf(p, "%i", &hwif_irq);
 
