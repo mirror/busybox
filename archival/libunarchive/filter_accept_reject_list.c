6b4377f3337d7d3e21e63aa1341d33fd51c2f97c
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2006-04-13 13:32:16 +0000

    - remove unneeded include, use shorter boilerplate while at it and also   shrink filter_accept_reject_list() a tiny bit while at it.    text    data     bss     dec     hex filename      59       0       0      59      3b filter_accept_reject_list.o      62       0       0      62      3e filter_accept_reject_list.o.orig

--- archival/libunarchive/filter_accept_reject_list.c
+++ archival/libunarchive/filter_accept_reject_list.c
@@ -1,24 +1,11 @@
+/* vi: set sw=4 ts=4: */
 /*
- *  Copyright (C) 2002 by Glenn McGrath
+ * Copyright (C) 2002 by Glenn McGrath
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * Licensed under GPLv2 or later, see file LICENSE in this tarball for details.
  */
 
-#include <fnmatch.h>
 #include <stdlib.h>
-
 #include "unarchive.h"
 
 /*
@@ -27,16 +14,17 @@
 char filter_accept_reject_list(archive_handle_t *archive_handle)
 {
 	const char *key = archive_handle->file_header->name;
-	const llist_t *accept_entry = find_list_entry(archive_handle->accept, key);
 	const llist_t *reject_entry = find_list_entry(archive_handle->reject, key);
+	const llist_t *accept_entry;
 
 	/* If the key is in a reject list fail */
 	if (reject_entry) {
 		return(EXIT_FAILURE);
 	}
+	accept_entry = find_list_entry(archive_handle->accept, key);
 
 	/* Fail if an accept list was specified and the key wasnt in there */
-	if (archive_handle->accept && (accept_entry == NULL)) {
+	if ((accept_entry == NULL) && archive_handle->accept) {
 		return(EXIT_FAILURE);
 	}
 
--- archival/libunarchive/find_list_entry.c
+++ archival/libunarchive/find_list_entry.c
@@ -1,28 +1,20 @@
+/* vi: set sw=4 ts=4: */
 /*
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * Copyright (C) 2002 by Glenn McGrath
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * Licensed under GPLv2 or later, see file LICENSE in this tarball for details.
  */
 
 #include <fnmatch.h>
 #include <stdlib.h>
 #include "unarchive.h"
 
+/* Find a string in a list */
 const llist_t *find_list_entry(const llist_t *list, const char *filename)
 {
 	while (list) {
 		if (fnmatch(list->data, filename, 0) == 0) {
-			return(list);
+			return (list);
 		}
 		list = list->link;
 	}
