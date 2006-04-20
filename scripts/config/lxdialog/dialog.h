f162183527874d0027af008dd14a6b21615d27de
Mike Frysinger <vapier@gentoo.org>
2006-04-20 04:00:11 +0000

    kill signed warnings

--- scripts/config/lxdialog/dialog.h
+++ scripts/config/lxdialog/dialog.h
@@ -170,7 +170,7 @@ int dialog_menu (const char *title, const char *prompt, int height, int width,
 int dialog_checklist (const char *title, const char *prompt, int height,
 		int width, int list_height, int item_no,
 		struct dialog_list_item ** items, int flag);
-extern unsigned char dialog_input_result[];
+extern char dialog_input_result[];
 int dialog_inputbox (const char *title, const char *prompt, int height,
 		int width, const char *init);
 
