73f54702bc5dba06c8cc736be13ea686195fe4e9
Rob Landley <rob@landley.net>
2006-05-01 00:53:40 +0000

    Fixup a build break if ENABLE_SHOW_USAGE switched off.  (Fallout from the compressed usage messages patch).

--- applets/applets.c
+++ applets/applets.c
@@ -6,23 +6,10 @@
  * isn't something I'm going to worry about...  If you wrote something
  * here, please feel free to acknowledge your work.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
  * Based in part on code from sash, Copyright (c) 1999 by David I. Bell
  * Permission has been granted to redistribute this code under the GPL.
  *
+ * Licensed under GPLv2 or later, see file License in this tarball for details.
  */
 
 #include <unistd.h>
@@ -39,6 +26,8 @@ static const char usage_messages[] =
 #include "applets.h"
 ;
 #undef MAKE_USAGE
+#else
+#define usage_messages 0
 #endif /* ENABLE_SHOW_USAGE */
 
 #undef APPLET
@@ -498,13 +487,3 @@ void run_applet_by_name (const char *name, int argc, char **argv)
 		exit ((*(applet_using->main)) (argc, argv));
 	}
 }
-
-
-/* END CODE */
-/*
-  Local Variables:
-  c-file-style: "linux"
-  c-basic-offset: 4
-  tab-width: 4
-End:
-*/
