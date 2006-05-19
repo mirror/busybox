4009ed91f7cf27074c2b30cc8dc4b865169c5aef
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2006-05-19 12:41:13 +0000

    - Rich Felker writes: SIGIOT is not defined in any standard i can find and it seems to be useless (alias for SIGABRT) on linux. i put it in #ifdef but it's probably best just to remove it and cut down the size a bit.

--- editors/vi.c
+++ editors/vi.c
@@ -2180,7 +2180,9 @@ static void core_sig(int sig)
 	signal(SIGQUIT, core_sig);
 	signal(SIGILL, core_sig);
 	signal(SIGTRAP, core_sig);
+#ifdef SIGIOT
 	signal(SIGIOT, core_sig);
+#endif
 	signal(SIGABRT, core_sig);
 	signal(SIGFPE, core_sig);
 	signal(SIGBUS, core_sig);
