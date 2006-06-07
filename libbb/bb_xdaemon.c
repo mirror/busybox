ca70774293a3ebb0f1ca4b0626b135cc74cfdea8
Mike Frysinger <vapier@gentoo.org>
2006-06-07 21:47:34 +0000

    bb_xdaemon() isnt available on no-mmu systems

--- libbb/bb_xdaemon.c
+++ libbb/bb_xdaemon.c
@@ -6,12 +6,14 @@
  *
  * Licensed under GPLv2 or later, see file LICENSE in this tarball for details.
  */
+
 #include <unistd.h>
 #include "libbb.h"
 
+#ifndef BB_NOMMU
 void bb_xdaemon(int nochdir, int noclose)
 {
 	if (daemon(nochdir, noclose))
 		bb_perror_msg_and_die("daemon");
 }
-
+#endif
