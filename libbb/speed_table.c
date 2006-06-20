57c1f73dd5aaabe22107320e3c21d22ae4313d60
Rob Landley <rob@landley.net>
2006-06-20 16:17:24 +0000

    Revert the last two patches to go back to a state before this file was controversial.

--- libbb/speed_table.c
+++ libbb/speed_table.c
@@ -7,33 +7,111 @@
  * Licensed under GPLv2 or later, see file LICENSE in this tarball for details.
  */
 
+#include <termios.h>
 #include "libbb.h"
 
-static const unsigned short speeds[] = {
-	0, 50, 75, 110, 134, 150, 200, 300, 600, 1200, 1800, 2400, 4800, 9600,
-   	19200, 38400, 57600>>8, 115200>>8, 230400>>8, 460800>>8, 500000>>8,
-	576000>>8, 921600>>8, 1000000>>8, 1152000>>8, 1500000>>8, 2000000>>8,
-	3000000>>8, 3500000>>8, 4000000>>8
+struct speed_map {
+	unsigned short speed;
+	unsigned short value;
 };
 
-unsigned int tty_baud_to_value(speed_t speed)
-{
-	int i;
+static const struct speed_map speeds[] = {
+	{B0, 0},
+	{B50, 50},
+	{B75, 75},
+	{B110, 110},
+	{B134, 134},
+	{B150, 150},
+	{B200, 200},
+	{B300, 300},
+	{B600, 600},
+	{B1200, 1200},
+	{B1800, 1800},
+	{B2400, 2400},
+	{B4800, 4800},
+	{B9600, 9600},
+#ifdef	B19200
+	{B19200, 19200},
+#elif defined(EXTA)
+	{EXTA, 19200},
+#endif
+#ifdef	B38400
+	{B38400, 38400/256 + 0x8000U},
+#elif defined(EXTB)
+	{EXTB, 38400/256 + 0x8000U},
+#endif
+#ifdef B57600
+	{B57600, 57600/256 + 0x8000U},
+#endif
+#ifdef B115200
+	{B115200, 115200/256 + 0x8000U},
+#endif
+#ifdef B230400
+	{B230400, 230400/256 + 0x8000U},
+#endif
+#ifdef B460800
+	{B460800, 460800/256 + 0x8000U},
+#endif
+};
 
-	for (i=0; i<sizeof(speeds) / sizeof(*speeds); i++)
-		if (speed == speeds[i] * (i>15 ? 256 : 1))
-			return i>15 ? (i+4096-14) : i;
+enum { NUM_SPEEDS = (sizeof(speeds) / sizeof(struct speed_map)) };
+
+unsigned long bb_baud_to_value(speed_t speed)
+{
+	int i = 0;
+
+	do {
+		if (speed == speeds[i].speed) {
+			if (speeds[i].value & 0x8000U) {
+				return ((unsigned long) (speeds[i].value) & 0x7fffU) * 256;
+			}
+			return speeds[i].value;
+		}
+	} while (++i < NUM_SPEEDS);
 
 	return 0;
 }
 
-speed_t tty_value_to_baud(unsigned int value)
+speed_t bb_value_to_baud(unsigned long value)
 {
-	int i;
+	int i = 0;
 
-	for (i=0; i<sizeof(speeds) / sizeof(*speeds); i++)
-		if (value == (i>15 ? (i+4096-14) : i))
-			return speeds[i] * (i>15 ? 256 : 1);
+	do {
+		if (value == bb_baud_to_value(speeds[i].speed)) {
+			return speeds[i].speed;
+		}
+	} while (++i < NUM_SPEEDS);
 
-	return -1;
+	return (speed_t) - 1;
 }
+
+#if 0
+/* testing code */
+#include <stdio.h>
+
+int main(void)
+{
+	unsigned long v;
+	speed_t s;
+
+	for (v = 0 ; v < 500000 ; v++) {
+		s = bb_value_to_baud(v);
+		if (s == (speed_t) -1) {
+			continue;
+		}
+		printf("v = %lu -- s = %0lo\n", v, (unsigned long) s);
+	}
+
+	printf("-------------------------------\n");
+
+	for (s = 0 ; s < 010017+1 ; s++) {
+		v = bb_baud_to_value(s);
+		if (!v) {
+			continue;
+		}
+		printf("v = %lu -- s = %0lo\n", v, (unsigned long) s);
+	}
+
+	return 0;
+}
+#endif
