42705abc2840b5472bf2c7846d83c75cfc695c1a
Mike Frysinger <vapier@gentoo.org>
2006-06-26 20:42:36 +0000

    dont setpgrp() for now as it breaks system() and that is more important than ctrl+c handling of background processes

--- shell/msh.c
+++ shell/msh.c
@@ -2970,10 +2970,6 @@ forkexec(REGISTER struct op *t, int *pin, int *pout, int act, char **wp)
 	if (resetsig) {
 		signal(SIGINT, SIG_DFL);
 		signal(SIGQUIT, SIG_DFL);
-	} else {
-		/* put non-interactive processes into a different process group.
-		 * we don't support jobs, but this is at least sane: see Bug 659 */
-		bb_setpgrp;
 	}
 
 	if (t->type == TPAREN)
