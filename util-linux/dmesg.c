aa516a61b994526e1b09fbe863ea34e553e76017
Rob Landley <rob@landley.net>
2006-07-06 16:41:56 +0000

    Fix three embarassing thinkos in the new dmesg.c: 1) the c argument shouldn't have had a : after that, dunno how that got there. 2) the xgetlarg for level was using size 3) because xgetlarg's error message _SUCKS_ (it does a show_usage() rather than giving any specific info about the range that was violated) I dropped the range down to 2 bytes.  (Which works fine, I dunno why we were nit-picking about that...)

--- util-linux/dmesg.c
+++ util-linux/dmesg.c
@@ -15,16 +15,16 @@
 int dmesg_main(int argc, char *argv[])
 {
 	char *size, *level;
-	int flags = bb_getopt_ulflags(argc, argv, "c:s:n:", &size, &level);
+	int flags = bb_getopt_ulflags(argc, argv, "cs:n:", &size, &level);
 
 	if (flags & 4) {
-		if(klogctl(8, NULL, bb_xgetlarg(size, 10, 0, 10)))
+		if(klogctl(8, NULL, bb_xgetlarg(level, 10, 0, 10)))
 			bb_perror_msg_and_die("klogctl");
 	} else {
 		int len;
 		char *buf;
 
-		len = (flags & 2) ? bb_xgetlarg(size, 10, 4096, INT_MAX) : 16384;
+		len = (flags & 2) ? bb_xgetlarg(size, 10, 2, INT_MAX) : 16384;
 		buf = xmalloc(len);
 		if (0 > (len = klogctl(3 + (flags & 1), buf, len)))
 			bb_perror_msg_and_die("klogctl");
