b988462d3e419559cf34c59d06323c59ba0a38df
Rob Landley <rob@landley.net>
2006-08-10 01:08:41 +0000

    Ok, features.h already defines _OPEN_SOURCE (and complains if it's already defined), yet it's not doing it for scripts/individual for some reason. (I hate fighting with header files.)

--- include/platform.h
+++ include/platform.h
@@ -154,7 +154,6 @@ __extension__ typedef unsigned long long __u64;
 
 #if defined __GLIBC__ || defined __UCLIBC__ \
 	|| defined __dietlibc__ || defined _NEWLIB_VERSION
-#define _XOPEN_SOURCE
 #include <features.h>
 #define HAVE_FEATURES_H
 #include <stdint.h>
