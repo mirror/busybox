7869c018b50b515d5cd21763a3676339f6960d00
Denis Vlasenko <vda.linux@googlemail.com>
2006-09-22 08:30:52 +0000

    ipcs: fix my recent breakage

--- util-linux/ipcs.c
+++ util-linux/ipcs.c
@@ -581,7 +581,7 @@ int ipcs_main(int argc, char **argv)
 
 	opt = bb_getopt_ulflags(argc, argv, "i:aqsmtcplu", &opt_i);
 	if (opt & 0x1) { // -i
-		id = atoi(optarg);
+		id = atoi(opt_i);
 		flags |= flag_print;
 	}
 	if (opt & 0x2) flags |= flag_msg | flag_sem | flag_shm; // -a
