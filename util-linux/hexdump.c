2dbeaa95ca2c22c5e37f62b01e4324a308f0140f
Denis Vlasenko <vda.linux@googlemail.com>
2006-09-23 13:31:46 +0000

    hexdump: deindent, almost fits into 80 columns now

--- util-linux/hexdump.c
+++ util-linux/hexdump.c
@@ -33,11 +33,11 @@ static void bb_dump_addfile(char *name)
 }
 
 static const char * const add_strings[] = {
-			"\"%07.7_ax \" 16/1 \"%03o \" \"\\n\"",		/* b */
-			"\"%07.7_ax \" 16/1 \"%3_c \" \"\\n\"",		/* c */
-			"\"%07.7_ax \" 8/2 \"  %05u \" \"\\n\"",	/* d */
-			"\"%07.7_ax \" 8/2 \" %06o \" \"\\n\"",		/* o */
-			"\"%07.7_ax \" 8/2 \"   %04x \" \"\\n\"",	/* x */
+	"\"%07.7_ax \" 16/1 \"%03o \" \"\\n\"",		/* b */
+	"\"%07.7_ax \" 16/1 \"%3_c \" \"\\n\"",		/* c */
+	"\"%07.7_ax \" 8/2 \"  %05u \" \"\\n\"",	/* d */
+	"\"%07.7_ax \" 8/2 \" %06o \" \"\\n\"",		/* o */
+	"\"%07.7_ax \" 8/2 \"   %04x \" \"\\n\"",	/* x */
 };
 
 static const char add_first[] = "\"%07.7_Ax\n\"";
@@ -53,7 +53,6 @@ static const struct suffix_mult suffixes[] = {
 
 int hexdump_main(int argc, char **argv)
 {
-//	register FS *tfs;
 	const char *p;
 	int ch;
 
@@ -61,34 +60,33 @@ int hexdump_main(int argc, char **argv)
 	bb_dump_length = -1;
 
 	while ((ch = getopt(argc, argv, hexdump_opts)) > 0) {
-		if ((p = strchr(hexdump_opts, ch)) != NULL) {
-			if ((p - hexdump_opts) < 5) {
-				bb_dump_add(add_first);
-				bb_dump_add(add_strings[(int)(p - hexdump_opts)]);
-			} else if (ch == 'C') {
-				bb_dump_add("\"%08.8_Ax\n\"");
-				bb_dump_add("\"%08.8_ax  \" 8/1 \"%02x \" \"  \" 8/1 \"%02x \" ");
-				bb_dump_add("\"  |\" 16/1 \"%_p\" \"|\\n\"");
-			} else {
-				/* Sae a little bit of space below by omitting the 'else's. */
-				if (ch == 'e') {
-					bb_dump_add(optarg);
-				} /* else */
-				if (ch == 'f') {
-					bb_dump_addfile(optarg);
-				} /* else */
-				if (ch == 'n') {
-					bb_dump_length = bb_xgetularg10_bnd(optarg, 0, INT_MAX);
-				} /* else */
-				if (ch == 's') {
-					bb_dump_skip = bb_xgetularg_bnd_sfx(optarg, 10, 0, LONG_MAX, suffixes);
-				} /* else */
-				if (ch == 'v') {
-					bb_dump_vflag = ALL;
-				}
-			}
-		} else {
+		p = strchr(hexdump_opts, ch)
+		if (!p)
 			bb_show_usage();
+		if ((p - hexdump_opts) < 5) {
+			bb_dump_add(add_first);
+			bb_dump_add(add_strings[(int)(p - hexdump_opts)]);
+		} else if (ch == 'C') {
+			bb_dump_add("\"%08.8_Ax\n\"");
+			bb_dump_add("\"%08.8_ax  \" 8/1 \"%02x \" \"  \" 8/1 \"%02x \" ");
+			bb_dump_add("\"  |\" 16/1 \"%_p\" \"|\\n\"");
+		} else {
+			/* Save a little bit of space below by omitting the 'else's. */
+			if (ch == 'e') {
+				bb_dump_add(optarg);
+			} /* else */
+			if (ch == 'f') {
+				bb_dump_addfile(optarg);
+			} /* else */
+			if (ch == 'n') {
+				bb_dump_length = bb_xgetularg10_bnd(optarg, 0, INT_MAX);
+			} /* else */
+			if (ch == 's') {
+				bb_dump_skip = bb_xgetularg_bnd_sfx(optarg, 10, 0, LONG_MAX, suffixes);
+			} /* else */
+			if (ch == 'v') {
+				bb_dump_vflag = ALL;
+			}
 		}
 	}
 
@@ -99,5 +97,5 @@ int hexdump_main(int argc, char **argv)
 
 	argv += optind;
 
-	return(bb_dump_dump(argv));
+	return bb_dump_dump(argv);
 }
