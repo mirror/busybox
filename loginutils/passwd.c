16c2c700fd26825fbd3e9591b5f590f5d2abf65d
Denis Vlasenko <vda.linux@googlemail.com>
2006-12-12 18:11:58 +0000

    passwd: fix indentation

--- loginutils/passwd.c
+++ loginutils/passwd.c
@@ -77,9 +77,8 @@ static char* new_password(const struct passwd *pw, uid_t myuid, int algo)
 	if (!newp)
 		goto err_ret;
 	newp = xstrdup(newp); /* we are going to bb_askpass() again, so save it */
-	if (obscure(orig, newp, pw) && myuid) {
-			goto err_ret; /* non-root is not allowed to have weak passwd */
-	}
+	if (obscure(orig, newp, pw) && myuid)
+		goto err_ret; /* non-root is not allowed to have weak passwd */
 
 	cp = bb_askpass(0, "Retype password:");
 	if (!cp)
