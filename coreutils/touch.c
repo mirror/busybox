d19f4aaa21b9530f59f51465df18a376c7ba30e6
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2007-01-20 21:32:38 +0000

    - small size tweak

--- coreutils/touch.c
+++ coreutils/touch.c
@@ -29,10 +29,8 @@
 int touch_main(int argc, char **argv)
 {
 	int fd;
-	int flags;
 	int status = EXIT_SUCCESS;
-
-	flags = getopt32(argc, argv, "c");
+	bool flags = (getopt32(argc, argv, "c") & 1);
 
 	argv += optind;
 
@@ -43,7 +41,7 @@ int touch_main(int argc, char **argv)
 	do {
 		if (utime(*argv, NULL)) {
 			if (errno == ENOENT) {	/* no such file*/
-				if (flags & 1) {	/* Creation is disabled, so ignore. */
+				if (flags) {	/* Creation is disabled, so ignore. */
 					continue;
 				}
 				/* Try to create the file. */
