c9c893d4f59418c50c8eb42bd80390026e123dd8
Denis Vlasenko <vda.linux@googlemail.com>
2007-03-12 23:41:07 +0000

    resize: make it usable in in backticks; have a timeout (if display doesn't respond to 'get cursor pos' ESC sequence...)

--- console-tools/resize.c
+++ console-tools/resize.c
@@ -11,31 +11,60 @@
 
 #define ESC "\033"
 
+struct termios old;
+
+static void
+onintr(int sig ATTRIBUTE_UNUSED)
+{
+	tcsetattr(STDERR_FILENO, TCSANOW, &old);
+	exit(1);
+}
+
+
 int resize_main(int argc, char **argv);
 int resize_main(int argc, char **argv)
 {
-	struct termios old, new;
-	struct winsize w = {0,0,0,0};
+	struct termios new;
+	struct winsize w = { 0,0,0,0 };
 	int ret;
 
-	tcgetattr(STDOUT_FILENO, &old); /* fiddle echo */
+	/* We use _stderr_ in order to make resize usable
+	 * in shell backticks (those redirect stdout away from tty).
+	 * NB: other versions of resize open "/dev/tty"
+	 * and operate on it - should we do the same?
+	 */
+
+	tcgetattr(STDERR_FILENO, &old); /* fiddle echo */
 	new = old;
 	new.c_cflag |= (CLOCAL | CREAD);
 	new.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-	tcsetattr(STDOUT_FILENO, TCSANOW, &new);
+	signal(SIGINT, onintr);
+	signal(SIGQUIT, onintr);
+	signal(SIGTERM, onintr);
+	signal(SIGALRM, onintr);
+	tcsetattr(STDERR_FILENO, TCSANOW, &new);
+
 	/* save_cursor_pos 7
 	 * scroll_whole_screen [r
 	 * put_cursor_waaaay_off [$x;$yH
 	 * get_cursor_pos [6n
 	 * restore_cursor_pos 8
 	 */
-	printf(ESC"7" ESC"[r" ESC"[999;999H" ESC"[6n");
+	fprintf(stderr, ESC"7" ESC"[r" ESC"[999;999H" ESC"[6n");
+	alarm(3); /* Just in case terminal won't answer */
 	scanf(ESC"[%hu;%huR", &w.ws_row, &w.ws_col);
-	ret = ioctl(STDOUT_FILENO, TIOCSWINSZ, &w);
-	printf(ESC"8");
-	tcsetattr(STDOUT_FILENO, TCSANOW, &old);
+	fprintf(stderr, ESC"8");
+
+	/* BTW, other versions of resize recalculate w.ws_xpixel, ws.ws_ypixel
+	 * by calculating character cell HxW from old values
+	 * (gotten via TIOCGWINSZ) and recomputing *pixel values */
+	ret = ioctl(STDERR_FILENO, TIOCSWINSZ, &w);
+
+	tcsetattr(STDERR_FILENO, TCSANOW, &old);
+
 	if (ENABLE_FEATURE_RESIZE_PRINT)
 		printf("COLUMNS=%d;LINES=%d;export COLUMNS LINES;\n",
 			w.ws_col, w.ws_row);
+
 	return ret;
 }
