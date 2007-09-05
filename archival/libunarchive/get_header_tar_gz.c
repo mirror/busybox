211f7f88ae67e875bed50409b2edbb6d740fc648
Denis Vlasenko <vda.linux@googlemail.com>
2007-09-05 11:48:32 +0000

    tar + gzip/bzip2/etc: support NOMMU machines (by Alex Landau <landau_alex@yahoo.com>)

--- archival/libunarchive/get_header_tar_gz.c
+++ archival/libunarchive/get_header_tar_gz.c
@@ -8,19 +8,26 @@
 
 char get_header_tar_gz(archive_handle_t *archive_handle)
 {
+#if BB_MMU
 	unsigned char magic[2];
+#endif
 
 	/* Can't lseek over pipes */
 	archive_handle->seek = seek_by_read;
 
+	/* Check gzip magic only if open_transformer will invoke unpack_gz_stream (MMU case).
+	 * Otherwise, it will invoke an external helper "gunzip -cf" (NOMMU case) which will
+	 * need the header. */
+#if BB_MMU
 	xread(archive_handle->src_fd, &magic, 2);
 	if ((magic[0] != 0x1f) || (magic[1] != 0x8b)) {
 		bb_error_msg_and_die("invalid gzip magic");
 	}
 
 	check_header_gzip_or_die(archive_handle->src_fd);
+#endif
 
-	archive_handle->src_fd = open_transformer(archive_handle->src_fd, unpack_gz_stream);
+	archive_handle->src_fd = open_transformer(archive_handle->src_fd, unpack_gz_stream, "gunzip", "gunzip", "-cf", "-", NULL);
 	archive_handle->offset = 0;
 	while (get_header_tar(archive_handle) == EXIT_SUCCESS) /**/;
 
--- archival/libunarchive/open_transformer.c
+++ archival/libunarchive/open_transformer.c
@@ -7,29 +7,48 @@
 #include "unarchive.h"
 
 /* transformer(), more than meets the eye */
+/*
+ * On MMU machine, the transform_prog and ... are stripped
+ * by a macro in include/unarchive.h. On NOMMU, transformer is stripped.
+ */
 int open_transformer(int src_fd,
-	USE_DESKTOP(long long) int (*transformer)(int src_fd, int dst_fd))
+	USE_DESKTOP(long long) int (*transformer)(int src_fd, int dst_fd),
+	const char *transform_prog, ...)
 {
 	int fd_pipe[2];
 	int pid;
 
 	xpipe(fd_pipe);
 
+#if BB_MMU
 	pid = fork();
-	if (pid == -1) {
+#else
+	pid = vfork();
+#endif
+	if (pid == -1)
 		bb_perror_msg_and_die("fork failed");
-	}
 
 	if (pid == 0) {
+#if !BB_MMU
+		va_list ap;
+#endif
 		/* child process */
 		close(fd_pipe[0]); /* We don't wan't to read from the parent */
 		// FIXME: error check?
+#if BB_MMU
 		transformer(src_fd, fd_pipe[1]);
 		if (ENABLE_FEATURE_CLEAN_UP) {
 			close(fd_pipe[1]); /* Send EOF */
 			close(src_fd);
 		}
 		exit(0);
+#else
+		xmove_fd(src_fd, 0);
+		xmove_fd(fd_pipe[1], 1);
+		va_start(ap, transform_prog);
+		BB_EXECVP(transform_prog, ap);
+		bb_perror_and_die("exec failed");
+#endif
 		/* notreached */
 	}
 
