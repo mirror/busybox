9b0a439f026adec773706625f080ab00ae537fa0
Denis Vlasenko <vda.linux@googlemail.com>
2007-09-20 19:44:10 +0000

    remove stray 8-bit chars from copyright banner

--- applets/applets.c
+++ applets/applets.c
@@ -553,8 +553,8 @@ static int busybox_main(char **argv)
 		output_width -= sizeof("start-stop-daemon, ") + 8;
 
 		printf("%s multi-call binary\n", bb_banner); /* reuse const string... */
-		printf("Copyright (C) 1998-2006  Erik Andersen, Rob Landley, and others.\n"
-		       "Licensed under GPLv2.  See source distribution for full notice.\n"
+		printf("Copyright (C) 1998-2006 Erik Andersen, Rob Landley, and others.\n"
+		       "Licensed under GPLv2. See source distribution for full notice.\n"
 		       "\n"
 		       "Usage: busybox [function] [arguments]...\n"
 		       "   or: [function] [arguments]...\n"
