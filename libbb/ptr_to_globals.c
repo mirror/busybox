93b8263652ac001247ff923499bdc04ab0a1a4c4
Denis Vlasenko <vda.linux@googlemail.com>
2008-04-11 11:27:29 +0000

    fix build with gcc -combine

--- libbb/ptr_to_globals.c
+++ libbb/ptr_to_globals.c
@@ -5,7 +5,20 @@
  * Licensed under GPLv2, see file LICENSE in this tarball for details.
  */
 
+struct globals;
+
+#ifndef GCC_COMBINE
+
 /* We cheat here. It is declared as const ptr in libbb.h,
  * but here we make it live in R/W memory */
-struct globals;
 struct globals *ptr_to_globals;
+
+#else
+
+/* gcc -combine will see through and complain */
+/* Using alternative method which is more likely to break
+ * on weird architectures, compilers, linkers and so on */
+struct globals *const ptr_to_globals __attribute__ ((section (".data")));
+
+#endif
+
