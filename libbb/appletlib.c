15ce7f5b58563c6b8183da9b5404e5b5d9ba8507
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2008-04-24 10:35:50 +0000

    - bump copyright year

--- libbb/appletlib.c
+++ libbb/appletlib.c
@@ -639,7 +639,7 @@ static int busybox_main(char **argv)
 
 		full_write2_str(bb_banner); /* reuse const string... */
 		full_write2_str(" multi-call binary\n"
-		       "Copyright (C) 1998-2007 Erik Andersen, Rob Landley, Denys Vlasenko\n"
+		       "Copyright (C) 1998-2008 Erik Andersen, Rob Landley, Denys Vlasenko\n"
 		       "and others. Licensed under GPLv2.\n"
 		       "See source distribution for full notice.\n"
 		       "\n"
