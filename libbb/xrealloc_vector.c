728161b5564afb94c5518fc440e8152f01e46068
Denis Vlasenko <vda.linux@googlemail.com>
2008-08-06 08:44:09 +0000

    fix bug 4464 for real. I am dumb.

--- libbb/xrealloc_vector.c
+++ libbb/xrealloc_vector.c
@@ -39,8 +39,7 @@ void* FAST_FUNC xrealloc_vector_helper(void *vector, unsigned sizeof_and_shift, 
 	if (!(idx & (mask - 1))) {
 		sizeof_and_shift >>= 8; /* sizeof(vector[0]) */
 		vector = xrealloc(vector, sizeof_and_shift * (idx + mask + 1));
-		vector = (char*)vector + (sizeof_and_shift * idx);
-		memset(vector, 0, sizeof_and_shift * (mask + 1));
+		memset((char*)vector + (sizeof_and_shift * idx), 0, sizeof_and_shift * (mask + 1));
 	}
 	return vector;
 }
