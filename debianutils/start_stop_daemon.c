75897ea6d50a479bb87f282ae91d3902da89d6bc
Denis Vlasenko <vda.linux@googlemail.com>
2008-09-27 01:05:13 +0000

    start_stop_daemon: accept (and ignore) -R <param>

--- debianutils/start_stop_daemon.c
+++ debianutils/start_stop_daemon.c
@@ -349,11 +349,11 @@ int start_stop_daemon_main(int argc UNUSED_PARAM, char **argv)
 	opt_complementary = "K:S:K--S:S--K:m?p:K?xpun:S?xa"
 		USE_FEATURE_START_STOP_DAEMON_FANCY("q-v");
 	opt = getopt32(argv, "KSbqtma:n:s:u:c:x:p:"
-		USE_FEATURE_START_STOP_DAEMON_FANCY("ovN:"),
-//		USE_FEATURE_START_STOP_DAEMON_FANCY("ovN:R:"),
+		USE_FEATURE_START_STOP_DAEMON_FANCY("ovN:R:"),
 		&startas, &cmdname, &signame, &userspec, &chuid, &execname, &pidfile
 		USE_FEATURE_START_STOP_DAEMON_FANCY(,&opt_N)
-//		USE_FEATURE_START_STOP_DAEMON_FANCY(,&retry_arg)
+		/* We accept and ignore -R <param> / --retry <param> */
+		USE_FEATURE_START_STOP_DAEMON_FANCY(,NULL)
 	);
 
 	if (opt & OPT_s) {
