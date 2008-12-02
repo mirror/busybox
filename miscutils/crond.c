b6ddee242592ffd02c05ae3ed6127c1dd80d7580
Denis Vlasenko <vda.linux@googlemail.com>
2008-12-02 21:52:24 +0000

    crond: put tasks in separate process groups

--- miscutils/crond.c
+++ miscutils/crond.c
@@ -779,6 +779,8 @@ ForkJob(const char *user, CronLine *line, int mailFd,
 			xmove_fd(mailFd, mail_filename ? 1 : 0);
 			dup2(1, 2);
 		}
+		/* "standard" cron puts tasks in separate process groups */
+		setpgrp();
 		execlp(prog, prog, cmd, arg, NULL);
 		crondlog(ERR20 "can't exec, user %s cmd %s %s %s", user, prog, cmd, arg);
 		if (mail_filename) {
@@ -914,6 +916,8 @@ static void RunJob(const char *user, CronLine *line)
 		if (DebugOpt) {
 			crondlog(LVL5 "child running %s", DEFAULT_SHELL);
 		}
+		/* "standard" cron puts tasks in separate process groups */
+		setpgrp();
 		execl(DEFAULT_SHELL, DEFAULT_SHELL, "-c", line->cl_Shell, NULL);
 		crondlog(ERR20 "can't exec, user %s cmd %s %s %s", user,
 				 DEFAULT_SHELL, "-c", line->cl_Shell);
