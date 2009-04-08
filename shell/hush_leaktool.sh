14725f82b61ecae6014c86762d3f2a768d573839
Denis Vlasenko <vda.linux@googlemail.com>
2009-04-08 16:46:45 +0000

    slight tweak to the leak detector

--- shell/hush_leaktool.sh
+++ shell/hush_leaktool.sh
@@ -5,9 +5,9 @@ output=output
 
 freelist=`grep 'free 0x' "$output" | cut -d' ' -f2 | sort | uniq | xargs`
 
-grep -v free "$output" >temp1
+grep -v free "$output" >"$output.leaked"
 for freed in $freelist; do
     echo Dropping $freed
-    grep -v $freed <temp1 >temp2
-    mv temp2 temp1
+    grep -v $freed <"$output.leaked" >"$output.temp"
+    mv "$output.temp" "$output.leaked"
 done
