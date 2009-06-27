ab0c8d7b35fbbe3e038800587cde0e3a99907187
Denys Vlasenko <vda.linux@googlemail.com>
2009-06-28 01:23:35 +0200

    wget: remove strange *&var construct.
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- networking/wget.c
+++ networking/wget.c
@@ -840,7 +840,7 @@ However, in real world it was observed that some web servers
 			 */
 			while ((str = gethdr(buf, sizeof(buf), sfp /*, &n*/)) != NULL) {
 				/* gethdr converted "FOO:" string to lowercase */
-				smalluint key = index_in_strings(keywords, *&buf) + 1;
+				smalluint key = index_in_strings(keywords, buf) + 1;
 				if (key == KEY_content_length) {
 					content_len = BB_STRTOOFF(str, NULL, 10);
 					if (errno || content_len < 0) {
