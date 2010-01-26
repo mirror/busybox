da879ec2e967e2f04f6269e506308dc9cc8b97ae
Denys Vlasenko <vda.linux@googlemail.com>
2010-01-26 08:17:45 +0100

    modprobe: protect against possible SEGV
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- modutils/modprobe.c
+++ modutils/modprobe.c
@@ -80,12 +80,14 @@ static int read_config(const char *path);
 static char *gather_options_str(char *opts, const char *append)
 {
 	/* Speed-optimized. We call gather_options_str many times. */
-	if (opts == NULL) {
-		opts = xstrdup(append);
-	} else {
-		int optlen = strlen(opts);
-		opts = xrealloc(opts, optlen + strlen(append) + 2);
-		sprintf(opts + optlen, " %s", append);
+	if (append) {
+		if (opts == NULL) {
+			opts = xstrdup(append);
+		} else {
+			int optlen = strlen(opts);
+			opts = xrealloc(opts, optlen + strlen(append) + 2);
+			sprintf(opts + optlen, " %s", append);
+		}
 	}
 	return opts;
 }
