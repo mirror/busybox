e91bc53d0c2e8de7dc4fbdb888ab0a4923c2b475
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2010-05-21 11:47:45 +0200

    lxdialog: fix ncursesw include detection
    the lib was checked but the include not, do so.
    TODO: check if upstream fixed that already or fix it there, too.
    
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

--- scripts/kconfig/lxdialog/check-lxdialog.sh
+++ scripts/kconfig/lxdialog/check-lxdialog.sh
@@ -19,7 +19,11 @@ ldflags()
 # Where is ncurses.h?
 ccflags()
 {
-	if [ -f /usr/include/ncurses/ncurses.h ]; then
+	if [ -f /usr/include/ncursesw/ncurses.h ]; then
+		echo '-I/usr/include/ncursesw -DCURSES_LOC="<ncurses.h>"'
+	elif [ -f /usr/include/ncursesw/curses.h ]; then
+		echo '-I/usr/include/ncursesw -DCURSES_LOC="<ncursesw/curses.h>"'
+	elif [ -f /usr/include/ncurses/ncurses.h ]; then
 		echo '-I/usr/include/ncurses -DCURSES_LOC="<ncurses.h>"'
 	elif [ -f /usr/include/ncurses/curses.h ]; then
 		echo '-I/usr/include/ncurses -DCURSES_LOC="<ncurses/curses.h>"'
