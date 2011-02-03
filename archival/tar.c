3237f5c307c066a008909f35115e21c9473504ab
Denys Vlasenko <vda.linux@googlemail.com>
2011-02-03 04:00:23 +0100

    tar: support -T - and -X -
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- archival/tar.c
+++ archival/tar.c
@@ -655,7 +655,7 @@ static llist_t *append_file_list_to_list(llist_t *list)
 	llist_t *newlist = NULL;
 
 	while (list) {
-		src_stream = xfopen_for_read(llist_pop(&list));
+		src_stream = xfopen_stdin(llist_pop(&list));
 		while ((line = xmalloc_fgetline(src_stream)) != NULL) {
 			/* kill trailing '/' unless the string is just "/" */
 			char *cp = last_char_is(line, '/');
