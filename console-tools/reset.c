b2f00ac22a24b68e74978d04f443cfa01c665fe9
Denys Vlasenko <vda.linux@googlemail.com>
2011-02-10 10:18:22 +0100

    reset: "ESC(K" selects German, why do we use THAT? Change to "ESC(B" (US)
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- console-tools/reset.c
+++ console-tools/reset.c
@@ -31,12 +31,12 @@ int reset_main(int argc UNUSED_PARAM, char **argv UNUSED_PARAM)
 	if (/*isatty(STDIN_FILENO) &&*/ isatty(STDOUT_FILENO)) {
 		/* See 'man 4 console_codes' for details:
 		 * "ESC c"        -- Reset
-		 * "ESC ( K"      -- Select user mapping
+		 * "ESC ( B"      -- Select G0 Character Set (B = US)
 		 * "ESC [ 0 m"    -- Reset all display attributes
 		 * "ESC [ J"      -- Erase to the end of screen
 		 * "ESC [ ? 25 h" -- Make cursor visible
 		 */
-		printf(ESC"c" ESC"(K" ESC"[0m" ESC"[J" ESC"[?25h");
+		printf(ESC"c" ESC"(B" ESC"[0m" ESC"[J" ESC"[?25h");
 		/* http://bugs.busybox.net/view.php?id=1414:
 		 * people want it to reset echo etc: */
 #if ENABLE_STTY
