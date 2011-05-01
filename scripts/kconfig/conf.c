fcbc641fe36a2ceff334362cc6ba62b000c842a5
Cristian Ionescu-Idbohrn <cii@axis.com>
2011-05-01 14:43:53 +0200

    get rid of several "variable 'x' set but not used" warnings
    Signed-off-by: Cristian Ionescu-Idbohrn <cii@axis.com>
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- scripts/kconfig/conf.c
+++ scripts/kconfig/conf.c
@@ -173,7 +173,7 @@ static void conf_askvalue(struct symbol *sym, const char *def)
 int conf_string(struct menu *menu)
 {
 	struct symbol *sym = menu->sym;
-	const char *def, *help;
+	const char *def;
 
 	while (1) {
 		printf("%*s%s ", indent - 1, "", menu->prompt->text);
@@ -188,10 +188,7 @@ int conf_string(struct menu *menu)
 		case '?':
 			/* print help */
 			if (line[1] == '\n') {
-				help = nohelp_text;
-				if (menu->sym->help)
-					help = menu->sym->help;
-				printf("\n%s\n", menu->sym->help);
+				printf("\n%s\n", menu->sym->help ? menu->sym->help : nohelp_text);
 				def = NULL;
 				break;
 			}
@@ -207,7 +204,6 @@ int conf_string(struct menu *menu)
 static int conf_sym(struct menu *menu)
 {
 	struct symbol *sym = menu->sym;
-	int type;
 	tristate oldval, newval;
 	const char *help;
 
@@ -215,7 +211,6 @@ static int conf_sym(struct menu *menu)
 		printf("%*s%s ", indent - 1, "", menu->prompt->text);
 		if (sym->name)
 			printf("(%s) ", sym->name);
-		type = sym_get_type(sym);
 		putchar('[');
 		oldval = sym_get_tristate_value(sym);
 		switch (oldval) {
@@ -282,11 +277,9 @@ static int conf_choice(struct menu *menu)
 {
 	struct symbol *sym, *def_sym;
 	struct menu *child;
-	int type;
 	bool is_new;
 
 	sym = menu->sym;
-	type = sym_get_type(sym);
 	is_new = !sym_has_value(sym);
 	if (sym_is_changable(sym)) {
 		conf_sym(menu);
