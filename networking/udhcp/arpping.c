6500d123d01af895d2b8eae1a36e553a1f75e211
Denys Vlasenko <vda.linux@googlemail.com>
2012-04-17 17:10:31 +0200

    udhcp: make arpping code resistant to time jumps
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- networking/udhcp/arpping.c
+++ networking/udhcp/arpping.c
@@ -118,8 +118,13 @@ int FAST_FUNC arpping(uint32_t test_nip,
 				break;
 			}
 		}
-		timeout_ms -= (unsigned)monotonic_ms() - prevTime;
-	} while (timeout_ms > 0);
+		timeout_ms -= (unsigned)monotonic_ms() - prevTime + 1;
+
+		/* We used to check "timeout_ms > 0", but
+		 * this is more under/overflow-resistant
+		 * (people did see overflows here when system time jumps):
+		 */
+	} while ((unsigned)timeout_ms <= 2000);
 
  ret:
 	close(s);
