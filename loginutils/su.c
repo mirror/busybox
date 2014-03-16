0e3334c8d8f021ed87d873ac95e165cde9da5e4d
Romain Naour <romain.naour@openwide.fr>
2014-03-16 11:18:19 +0100

    su: add a delay if the password is incorrect
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- loginutils/su.c
+++ loginutils/su.c
@@ -101,6 +101,7 @@ int su_main(int argc UNUSED_PARAM, char **argv)
 		if (ENABLE_FEATURE_SU_SYSLOG)
 			syslog(LOG_NOTICE, "%c %s %s:%s",
 				'-', tty, old_user, opt_username);
+		bb_do_delay(LOGIN_FAIL_DELAY);
 		bb_error_msg_and_die("incorrect password");
 	}
 
