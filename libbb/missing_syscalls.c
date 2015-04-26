93b98ff5726fd620e1f123d04072b956412c1b55
Matt Whitlock <busybox@mattwhitlock.name>
2015-04-26 13:14:50 +0200

    Bionic lacks tcdrain; provide a workaround
    Signed-off-by: Matt Whitlock <busybox@mattwhitlock.name>
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- libbb/missing_syscalls.c
+++ libbb/missing_syscalls.c
@@ -39,4 +39,9 @@ int pivot_root(const char *new_root, const char *put_old)
 {
 	return syscall(__NR_pivot_root, new_root, put_old);
 }
+
+int tcdrain(int fd)
+{
+	return ioctl(fd, TCSBRK, 1);
+}
 #endif
