3b650c1e7b0bcbb4dfebab6fd87449e6b1e0b788
Denys Vlasenko <vda.linux@googlemail.com>
2015-10-05 16:46:29 +0200

    qemu testing: extract_od_binary.sh: support huge files (and broken od)
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- qemu_multiarch_testing/extract_od_binary.sh
+++ qemu_multiarch_testing/extract_od_binary.sh
@@ -3,4 +3,4 @@
 # Converts textual result of "od -tx1 <FILE"
 # back into a binary FILE
 
-grep -a '^[0-7][0-7][0-7][0-7][0-7][0-7][0-7] [0-9a-f][0-9a-f] [0-9a-f][0-9a-f] [0-9a-f][0-9a-f] [0-9a-f][0-9a-f]' | busybox hexdump -R
+grep -a '^[0-7][0-7][0-7][0-7][0-7][0-7][0-7][0-7]* [0-9a-f][0-9a-f] [0-9a-f][0-9a-f] [0-9a-f][0-9a-f] [0-9a-f][0-9a-f]' | busybox hexdump -R
