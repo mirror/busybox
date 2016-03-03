4c48a6474701d8b396a14213ebcc979a49187faf
Denys Vlasenko <vda.linux@googlemail.com>
2016-03-03 22:01:23 +0100

    ntpd: more informative poll lowering message
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- networking/ntpd.c
+++ networking/ntpd.c
@@ -1953,8 +1953,8 @@ recv_and_process_peer_pkt(peer_t *p)
 			adjust_poll(MINPOLL);
 		} else {
 			VERB3 if (rc > 0)
-				bb_error_msg("want smaller poll interval: offset/jitter > %u",
-					POLLADJ_GATE);
+				bb_error_msg("want smaller interval: offset/jitter = %u",
+					G.offset_to_jitter_ratio);
 			adjust_poll(-G.poll_exp * 2);
 		}
 	}
