ca26f1c238c51b3b2f80dd25fd7fcf8774f38ca4
Denys Vlasenko <vda.linux@googlemail.com>
2016-08-19 20:22:45 +0200

    hush: fix a bug with "stdio rewind on exit". Closes 9186
    With FEATURE_SH_STANDALONE=y, run this in a "sh SCRIPT":
    
    sha256sum /dev/null
    echo END
    
    sha256sum is a NOEXEC applet. It runs in a forked child. Then child exit()s.
    By this time, entire script is read, and buffered in a FILE object
    from fopen("SCRIPT"). But fgetc() did not consume entire input.
    exit() lseeks back by -9 bytes, from <eof> to 'e' in 'echo'.
    (this may be libc-specific).
    This change of fd position *is shared with the parent*!
    
    Now parent can read more, and it thinks there is another "echo END".
    End result: two "echo END"s are run.
    
    `cmd` and arithmetic also need the fix for this, even without
    FEATURE_SH_STANDALONE.
    
    Fix this by _exit()ing instead.
    
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- libbb/appletlib.c
+++ libbb/appletlib.c
@@ -771,7 +771,9 @@ void FAST_FUNC run_applet_no_and_exit(int applet_no, char **argv)
 	}
 	if (ENABLE_FEATURE_SUID)
 		check_suid(applet_no);
-	exit(applet_main[applet_no](argc, argv));
+	xfunc_error_retval = applet_main[applet_no](argc, argv);
+	/* Note: applet_main() may also not return (die on a xfunc or such) */
+	xfunc_die();
 }
 
 void FAST_FUNC run_applet_and_exit(const char *name, char **argv)
