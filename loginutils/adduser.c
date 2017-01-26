4d417709b0fb837fd6884a9bc5a55027dec8a985
Denys Vlasenko <vda.linux@googlemail.com>
2017-01-26 18:54:12 +0100

    adduser: help for -G GRP is misleading, fix it
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- loginutils/adduser.c
+++ loginutils/adduser.c
@@ -64,7 +64,7 @@
 //usage:     "\n	-h DIR		Home directory"
 //usage:     "\n	-g GECOS	GECOS field"
 //usage:     "\n	-s SHELL	Login shell"
-//usage:     "\n	-G GRP		Add user to existing group"
+//usage:     "\n	-G GRP		Group"
 //usage:     "\n	-S		Create a system user"
 //usage:     "\n	-D		Don't assign a password"
 //usage:     "\n	-H		Don't create home directory"
