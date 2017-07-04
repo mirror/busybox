d1f222c3b00839b289f5d04a69b3c704101d17b2
Denys Vlasenko <vda.linux@googlemail.com>
2017-07-04 16:01:12 +0200

    Change BB_EXTRA_VERSION: now it needs to contain any spaces/parenthesis
    Before this change, BB_EXTRA_VERSION of "" resulted in:
    
    	"BusyBox v1.28.0.git () multi-call binary"
    
    message, after the fix it is:
    
    	"BusyBox v1.28.0.git multi-call binary"
    
    While at it, eliminate BB_BT and BANNER single-use macros.
    
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- libbb/messages.c
+++ libbb/messages.c
@@ -14,12 +14,10 @@
 
 /* allow version to be extended, via CFLAGS */
 #ifndef BB_EXTRA_VERSION
-#define BB_EXTRA_VERSION BB_BT
+#define BB_EXTRA_VERSION " ("AUTOCONF_TIMESTAMP")"
 #endif
 
-#define BANNER "BusyBox v" BB_VER " (" BB_EXTRA_VERSION ")"
-
-const char bb_banner[] ALIGN1 = BANNER;
+const char bb_banner[] ALIGN1 = "BusyBox v" BB_VER BB_EXTRA_VERSION;
 
 
 const char bb_msg_memory_exhausted[] ALIGN1 = "out of memory";
