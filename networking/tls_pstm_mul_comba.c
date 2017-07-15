0a53eadea528673a813c5f9312b1f53130429f7c
Denys Vlasenko <vda.linux@googlemail.com>
2017-07-15 17:13:08 +0200

    tls: fix pstm asm constraint problem
    function                                             old     new   delta
    pstm_sqr_comba                                       551     475     -76
    
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- networking/tls_pstm_mul_comba.c
+++ networking/tls_pstm_mul_comba.c
@@ -85,7 +85,8 @@ asm(                                                      \
 	 "addl  %%eax,%0     \n\t"                            \
 	 "adcl  %%edx,%1     \n\t"                            \
 	 "adcl  $0,%2        \n\t"                            \
-	 :"=r"(c0), "=r"(c1), "=r"(c2): "0"(c0), "1"(c1), "2"(c2), "m"(i), "m"(j)  :"%eax","%edx","%cc");
+	 :"=rm"(c0), "=rm"(c1), "=rm"(c2): "0"(c0), "1"(c1), "2"(c2), "m"(i), "m"(j)  :"%eax","%edx","%cc");
+	//bbox: ^^^ replaced "=r" with "=rm": %ebx is not available on shared build
 
 /******************************************************************************/
 #elif defined(PSTM_X86_64)
