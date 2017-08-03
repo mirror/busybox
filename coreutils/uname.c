663ae52676eae3b0fdc6bb968ff6497279a034a4
Denys Vlasenko <vda.linux@googlemail.com>
2017-08-03 03:46:14 +0200

    config: FEDORA_COMPAT option (so far only tweaks uname)
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- coreutils/uname.c
+++ coreutils/uname.c
@@ -183,7 +183,7 @@ int uname_main(int argc UNUSED_PARAM, char **argv UNUSED_PARAM)
 		strcpy(uname_info.processor, unknown_str);
 		strcpy(uname_info.platform, unknown_str);
 		strcpy(uname_info.os, CONFIG_UNAME_OSNAME);
-# if 0
+# if ENABLE_FEDORA_COMPAT
 		/* Fedora does something like this */
 		strcpy(uname_info.processor, uname_info.name.machine);
 		strcpy(uname_info.platform, uname_info.name.machine);
