9c8e94bc0a3836dd6b8acbcf1fa88283a0a3c148
Ingo van Lil <inguin@gmx.de>
2018-01-05 15:04:23 +0100

    ash: fail if 'shift' operand is out of range
    If the numeric argument passed to ash's 'shift' built-in is greater than
    '$#' the command performs no operation and exits successfully. It should
    return a non-zero exit code instead:
    
    http://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html#shift
    
    This is consistent with bash and hush.
    
    function                                             old     new   delta
    shiftcmd                                             122     120      -2
    
    Signed-off-by: Ingo van Lil <inguin@gmx.de>
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- shell/ash.c
+++ shell/ash.c
@@ -10858,7 +10858,7 @@ shiftcmd(int argc UNUSED_PARAM, char **argv)
 	if (argv[1])
 		n = number(argv[1]);
 	if (n > shellparam.nparam)
-		n = 0; /* bash compat, was = shellparam.nparam; */
+		return 1;
 	INT_OFF;
 	shellparam.nparam -= n;
 	for (ap1 = shellparam.p; --n >= 0; ap1++) {
