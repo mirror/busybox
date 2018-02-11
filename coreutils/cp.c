mirror/busybox

commit 22542eca18e5807b72ddc78999f5101e33f17a53

getopt32: remove opt_complementary
function                                             old     new   delta
vgetopt32                                           1318    1392     +74
runsvdir_main                                        703     713     +10
bb_make_directory                                    423     425      +2
collect_cpu                                          546     545      -1
opt_chars                                              3       -      -3
opt_complementary                                      4       -      -4
tftpd_main                                           567     562      -5
ntp_init                                             476     471      -5
zcip_main                                           1266    1256     -10
xxd_main                                             428     418     -10
whois_main                                           140     130     -10
who_main                                             463     453     -10
which_main                                           212     202     -10
wget_main                                           2535    2525     -10
watchdog_main                                        291     281     -10
watch_main                                           222     212     -10
vlock_main                                           399     389     -10
uuencode_main                                        332     322     -10
uudecode_main                                        316     306     -10
unlink_main                                           45      35     -10
udhcpd_main                                         1482    1472     -10
udhcpc_main                                         2762    2752     -10
tune2fs_main                                         290     280     -10
tunctl_main                                          366     356     -10
truncate_main                                        218     208     -10

coreutils/cp.c
@@ -73,15 +73,17 @@ int cp_main(int argc, char **argv)
 #endif
 	};
 
-	// Need at least two arguments
-	// Soft- and hardlinking doesn't mix
-	// -P and -d are the same (-P is POSIX, -d is GNU)
-	// -r and -R are the same
-	// -R (and therefore -r) turns on -d (coreutils does this)
-	// -a = -pdR
-	opt_complementary = "-2:l--s:s--l:Pd:rRd:Rd:apdR";
 #if ENABLE_FEATURE_CP_LONG_OPTIONS
-	flags = getopt32long(argv, FILEUTILS_CP_OPTSTR,
+	flags = getopt32long(argv, "^"
+		FILEUTILS_CP_OPTSTR
+		"\0"
+		// Need at least two arguments
+		// Soft- and hardlinking doesn't mix
+		// -P and -d are the same (-P is POSIX, -d is GNU)
+		// -r and -R are the same
+		// -R (and therefore -r) turns on -d (coreutils does this)
+		// -a = -pdR
+		"-2:l--s:s--l:Pd:rRd:Rd:apdR",
 		"archive\0"        No_argument "a"
 		"force\0"          No_argument "f"
 		"interactive\0"    No_argument "i"

commit 7f441403cbcb6cce864bae66e1fc7bb626954e04

cp: fix option handling in non-longopt config
the patch getopt32: remove opt_complementary 22542eca18e5807b72ddc78999f5101e33f17a53

introduced a regressed in the cp command since it removed all aliases of arguments
if long_opts is not configured.

Patch by Sebastian Gottschall <s.gottschall@dd-wrt.com>

Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

coreutils/cp.c
@@ -101,7 +101,11 @@ int cp_main(int argc, char **argv)
 		"parents\0"        No_argument "\xfe"
 	);
 #else
-	flags = getopt32(argv, FILEUTILS_CP_OPTSTR);
+	flags = getopt32(argv, "^"
+		FILEUTILS_CP_OPTSTR
+		"\0"
+		"-2:l--s:s--l:Pd:rRd:Rd:apdR"
+	);
 #endif
 	/* Options of cp from GNU coreutils 6.10:
 	 * -a, --archive