mirror/busybox

commit 2da9724b56169f00bd7fb6b9a11c9409a7620981

libbb: remove unnecessary variable in xmalloc_fgets
Signed-off-by: Quentin Rameau <quinq@fifth.space>
Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

libbb/get_line_from_file.c
@@ -44,9 +44,7 @@ char* FAST_FUNC bb_get_chunk_from_file(FILE *file, int *end)
 /* Get line, including trailing \n if any */
 char* FAST_FUNC xmalloc_fgets(FILE *file)
 {
-	int i;
-
-	return bb_get_chunk_from_file(file, &i);
+	return bb_get_chunk_from_file(file, NULL);
 }
 /* Get line.  Remove trailing \n */
 char* FAST_FUNC xmalloc_fgetline(FILE *file)

commit e0f617699f7f1dfa1e56ded54bb1d70fd91c225e

head: add test case for negative -n invocation
Commit 2da9724b56169f00bd7fb6b9a11c9409a7620981 broke 'head -n -1' and was
later reverted with 0d598ab9f03dbf320f7b81c05e4a94cb303dfbc7.

This commit adds a test case to avoid future breakage.

Signed-off-by: Thomas De Schampheleire <thomas.de_schampheleire@nokia.com>
Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

testsuite/head.tests
@@ -1,0 +1,31 @@
+#!/bin/sh
+# Copyright 2018 Thomas De Schampheleire <thomas.de_schampheleire@nokia.com>
+# Licensed under GPLv2 or later, see file LICENSE in this source tree.
+
+. ./testing.sh
+
+# testing "test name" "command" "expected result" "file input" "stdin"
+
+cat <<EOF > head.input
+line 1
+line 2
+line 3
+line 4
+line 5
+line 6
+line 7
+line 8
+line 9
+line 10
+line 11
+line 12
+EOF
+
+testing "head -n <negative number>" \
+	"head -n -9 head.input" \
+	"line 1\nline 2\nline 3\n" \
+	"" ""
+
+rm head.input
+
+exit $FAILCOUNT