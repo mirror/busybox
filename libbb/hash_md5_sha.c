f69f207490dd25b89380c21e816b1f6644a7529f
Denys Vlasenko <vda.linux@googlemail.com>
2018-11-26 13:00:28 +0100

    libbb: add comment on sha384
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- libbb/hash_md5_sha.c
+++ libbb/hash_md5_sha.c
@@ -794,6 +794,11 @@ static const uint32_t init512_lo[] = {
 };
 #endif /* NEED_SHA512 */
 
+// Note: SHA-384 is identical to SHA-512, except that initial hash values are
+// 0xcbbb9d5dc1059ed8, 0x629a292a367cd507, 0x9159015a3070dd17, 0x152fecd8f70e5939,
+// 0x67332667ffc00b31, 0x8eb44a8768581511, 0xdb0c2e0d64f98fa7, 0x47b5481dbefa4fa4,
+// and the output is constructed by omitting last two 64-bit words of it.
+
 /* Initialize structure containing state of computation.
    (FIPS 180-2:5.3.2)  */
 void FAST_FUNC sha256_begin(sha256_ctx_t *ctx)
