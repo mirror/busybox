8b710ef000c383f3476245b27bacf034532d9786
Denys Vlasenko <vda.linux@googlemail.com>
2018-12-30 20:07:54 +0100

    make_single_applets.sh: fix for BSS trick
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- make_single_applets.sh
+++ make_single_applets.sh
@@ -72,6 +72,16 @@ for app; do
 		echo "NUM_APPLETS != 1 for ${app}: `cat include/NUM_APPLETS.h`"
 		mv .config busybox_config_${app}
 	else
+		if grep -q 'use larger COMMON_BUFSIZE' busybox_make_${app}.log; then
+			# FEATURE_USE_BSS_TAIL=y is selected, and build system
+			# recommends rebuilding. Do so, and print some
+			# debug info to see whether it works right:
+			tail -n1 busybox_make_${app}.log
+			nm busybox_unstripped | grep ' _end'
+			make >/dev/null 2>&1
+			nm busybox_unstripped | grep ' _end'
+			grep ^bb_common_bufsiz1 busybox_unstripped.map
+		fi
 		grep -i -e error: -e warning: busybox_make_${app}.log \
 		|| rm busybox_make_${app}.log
 		mv busybox busybox_${app}
