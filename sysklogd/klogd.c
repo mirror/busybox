92d84c40140c2681421ad005f971b8e5e6a96915
Alex Samorukov <samm@os2.kiev.ua>
2021-01-04 01:11:57 +0100

    Fix klogd applet compilation on the FreeBSD
    FreeBSD also using /dev/klog, see syslogd(8)
    
    Signed-off-by: Alex Samorukov <samm@os2.kiev.ua>
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- sysklogd/klogd.c
+++ sysklogd/klogd.c
@@ -100,7 +100,7 @@ static void klogd_close(void)
 #else
 
 # ifndef _PATH_KLOG
-#  ifdef __GNU__
+#  if defined(__GNU__) || defined (__FreeBSD__)
 #   define _PATH_KLOG "/dev/klog"
 #  else
 #   error "your system's _PATH_KLOG is unknown"
