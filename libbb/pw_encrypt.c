09aba8bac466ce8fb94a6b5ee587d81415ff4c44
Alex Samorukov <samm@os2.kiev.ua>
2021-01-04 01:36:05 +0100

    Fix missing "crypt.h" compilation error on the FreeBSD
    FreeBSD does not use crypt.h, but unistd.h which is already included
    
    Signed-off-by: Alex Samorukov <samm@os2.kiev.ua>
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- libbb/pw_encrypt.c
+++ libbb/pw_encrypt.c
@@ -7,7 +7,9 @@
  * Licensed under GPLv2 or later, see file LICENSE in this source tree.
  */
 #if !ENABLE_USE_BB_CRYPT
-#include <crypt.h>
+# if !defined(__FreeBSD__)
+#  include <crypt.h>
+# endif
 #endif
 #include "libbb.h"
 
