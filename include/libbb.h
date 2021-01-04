4a4b453a87da91a5051a1b7be9061861ec5c01a1
Alex Samorukov <samm@os2.kiev.ua>
2021-01-04 19:35:09 +0100

    Fix for the FEATURE_UTMP on the FreeBSD
    FreeBSD is not using <utmp.h> and does not define _PATH_UTMPX.
    Tested with busybox applets depending on FEATURE_UTMP (e.g. who, users, etc)
    
    Signed-off-by: Alex Samorukov <samm@os2.kiev.ua>
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- include/libbb.h
+++ include/libbb.h
@@ -106,7 +106,11 @@
 #  define updwtmpx updwtmp
 #  define _PATH_UTMPX _PATH_UTMP
 # else
-#  include <utmp.h>
+#  if !defined(__FreeBSD__)
+#   include <utmp.h>
+#  else
+#   define _PATH_UTMPX "/var/run/utx.active"
+#  endif
 #  include <utmpx.h>
 #  if defined _PATH_UTMP && !defined _PATH_UTMPX
 #   define _PATH_UTMPX _PATH_UTMP
